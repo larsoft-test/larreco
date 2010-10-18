86b1f3e0443f31f1e239ada5f1c1e337ff08f5c4
Kinga Partyka <kpartyka@fnal.gov>
2010-10-18 15:17:09 +0000

    adding plugin file for dbscan
    Former-commit-id: ec67bf3eb376b704729ef7e2c728674518d49888

---
+++ ClusterFinder/DBSCANfinder_plugin.cc
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: DBSCANfindercxx,
+//
+// 
+//
+// \author kinga.partyka@yale.edu
+//
+////////////////////////////////////////////////////////////////////////
+// Framework includes
+#include "FWCore/Framework/interface/MakerMacros.h" 
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h" 
+
+
+#include "ClusterFinder/DBSCANfinder.h"
+
+namespace cluster{
+
+  DEFINE_FWK_MODULE(DBcluster);
+  
+} 
