75d01249f29d42c47f09afa6e7d437ac8f287264
Kinga Partyka <kpartyka@fnal.gov>
2010-10-22 15:59:52 +0000

    replacing with corrected files
    Former-commit-id: ca6377a912826440aeca7926670b81ba210745b1

---
+++ ClusterFinder/DBclusterAna.h
@@ -0,0 +1,90 @@
+////////////////////////////////////////////////////////////////////////
+//
+// 
+// \author kinga.partyka@yale.edu
+//
+// 
+////////////////////////////////////////////////////////////////////////
+#ifndef DBCLUSTERANA_H
+#define DBCLUSTERANA_H
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "RecoBase/Wire.h"
+#include "RecoBase/Hit.h"
+#include "RecoBase/Cluster.h"
+#include "RawData/DAQHeader.h"
+
+#include <vector>
+#include <string>
+
+
+class TH1F;
+class TH2F;
+///Cluster finding and building 
+namespace cluster {
+
+   
+  class DBclusterAna : public edm::EDAnalyzer {
+
+  public:
+          
+    explicit DBclusterAna(edm::ParameterSet const& pset); 
+    virtual ~DBclusterAna();
+ 
+    /// read access to event
+      void analyze(const edm::Event& evt,  edm::EventSetup const&);
+    // analyze() Must go into new module. EC, 5-Oct-2010.
+    //    void analyze(const edm::Event& evt);
+      void beginJob(edm::EventSetup const&);
+
+  private:
+    TH1F* fNoParticles_pdg;
+    TH1F* fNoParticles_trackid; 
+    TH1F* fNoParticles_trackid_mother;
+    TH1F* fNoParticles_trackid_per_event;  
+    TH1F* fNoParticles_pdg_per_event;
+    TH1F* fCl_for_Muon;
+   /*  TH1F* fCl_for_Electron; */
+/*     TH1F* fCl_for_Positron; */
+/*     TH1F* fCl_for_Pion_111; */
+/*     TH1F* fCl_for_Pion_211; */
+/*     TH1F* fCl_for_Pion_m211; */
+/*     TH1F* fCl_for_Proton; */
+    TH1F* fNoClustersInEvent;
+    TH1F* fPercentNoise;
+    TH1F* fno_of_clusters_per_track;
+    TH1F* fPercent_lost_muon_hits;
+    TH1F* fPercent_lost_electron_hits;
+    TH1F* fPercent_lost_positron_hits;
+    TH1F* fPercent_lost_111_hits;
+    TH1F* fPercent_lost_211_hits;
+    TH1F* fPercent_lost_m211_hits;
+    TH1F* fPercent_lost_2212_hits;
+    TH1F* fPercent_lost_2112_hits;
+
+    TH1F* fPercent_lost_muon_energy;
+    TH1F* fPercent_lost_electron_energy;
+    TH1F* fPercent_lost_positron_energy;
+    TH1F* fPercent_lost_111_energy;
+    TH1F* fPercent_lost_211_energy;
+    TH1F* fPercent_lost_m211_energy;
+    TH1F* fPercent_lost_2212_energy;
+    TH1F* fPercent_lost_2112_energy;
+    TH1F* fEnergy;
+    TH2F* fbrian_in;
+    TH2F* fbrian_coll;
+    std::string fEvtModuleLabel;
+    std::string fDigitModuleLabel;
+    std::string fHitsModuleLabel;
+    std::string fLArG4ModuleLabel;
+    std::string fClusterFinderModuleLabel;
+    std::string fDetSimModuleLabel;
+    std::string fGenieGenModuleLabel;
+
+    
+      	 
+  }; // class DBclusterAna
+
+}
+
+#endif 
---
+++ ClusterFinder/DBclusterAna_plugin.cc
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: DBSCANfinderAna.cxx,v 1.36 2010/09/15  bpage Exp $
+//
+// DBSCANfinderAna class
+//
+// \author kinga.partyka@yale.edu
+//
+////////////////////////////////////////////////////////////////////////
+// Framework includes
+#include "FWCore/Framework/interface/MakerMacros.h" 
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h" 
+
+#include "DBclusterAna.h"
+
+namespace cluster{
+
+  DEFINE_FWK_MODULE(DBclusterAna);
+  
+} 
+
