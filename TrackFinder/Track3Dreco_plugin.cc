63b5f6482b338e1131e8ca30cf091c41078f4175
Eric Church <echurch@fnal.gov>
2010-12-07 23:52:15 +0000

    Need the plugin
    Former-commit-id: cb35c947d239fb64045881f0a5ac57e25338e2af

---
+++ TrackFinder/Track3Dreco_plugin.cc
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: Track3Dreco.cxx,v 1.36 2010/09/15 soderberg Exp $
+//
+// Track3Dreco class
+//
+// echurch@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+// Framework includes
+#include "FWCore/Framework/interface/MakerMacros.h" 
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h" 
+
+
+#include "TrackFinder/Track3Dreco.h"
+
+namespace trkf{
+
+  DEFINE_FWK_MODULE(Track3Dreco);
+  
+} // end namespace 
+
