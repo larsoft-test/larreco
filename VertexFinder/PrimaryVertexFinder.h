862d90e63c40b5cfcab70e53484e10204f128ad3
Saima Farooq <saima@fnal.gov>
2011-03-28 01:08:08 +0000

    minor changes in a function's definition
    Former-commit-id: 0cfc3516f50fd6beb7dc4d496c3ff54b22daf2d4

--- VertexFinder/PrimaryVertexFinder.h
+++ VertexFinder/PrimaryVertexFinder.h
@@ -35,7 +35,7 @@ namespace vertex {
     std::string fTrackModuleLabel;
     double      fVertexWindow;
     double      StartPointSeperation(recob::SpacePoint sp1, recob::SpacePoint sp2);
-    bool        IsInVertexCollection(int a, int b, std::vector<std::vector<int> > vertex_collection);
+    bool        IsInVertexCollection(int a, std::vector<std::vector<int> > vertex_collection);
     int         IndexInVertexCollection(int a, int b, std::vector<std::vector<int> > vertex_collection);
     bool        IsInNewVertex(int a, std::vector<int> newvertex);
     
