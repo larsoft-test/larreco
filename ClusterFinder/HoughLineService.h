93667b2696e501f3dfdbec932b386f1ed1ea4d90
Brian Rebel <brebel@fnal.gov>
2011-05-16 02:30:52 +0000

    move definition of HoughTransform out of the definition of the HoughLineService so that it can be used more easily without the HoughLineService.  Improve readability of the code by breaking up long lines and comments.  Add \todo tags for hard coded numbers, etc.  Also put initial { on the same lines as while, for, if statements as that improves readability of the code (believe it or not there have been studies of this sort of thing).
    Former-commit-id: cdc0596601bfcf4285a74c9d700b0002a7b2bac6

--- ClusterFinder/HoughLineService.h
+++ ClusterFinder/HoughLineService.h
@@ -22,6 +22,50 @@ namespace recob { class Cluster; }
 
 namespace cluster {
    
+  class HoughTransform {
+  public:
+    
+    HoughTransform();
+    ~HoughTransform();
+    
+    
+    void Init(int dx, int dy, int rhoresfact, int numACells);
+    bool AddPoint(int x, int y);
+    int  GetCell(int row, int col)            { return m_accum[row][col]; }
+    void SetCell(int row, int col, int value) { m_accum[row][col] = value; }
+    void IncrementCell(int row, int col)      { m_accum[row][col]++;}
+    void GetAccumSize(int &numRows, int &numCols) 
+    { 
+      numRows = m_accum.size();
+      numCols  = m_rowLength;
+    }
+    int NumAccumulated()                      { return m_numAccumulated; }
+    void GetEquation(double row, double col, double &rho, double &theta)
+    {
+      theta = (TMath::Pi()*row)/m_numAngleCells;
+      rho   = (col - (m_rowLength/2.))/m_rhoResolutionFactor;
+    }
+    int GetMax(int & xmax, int & ymax);
+      
+    private:
+         
+    int m_dx;
+    int m_dy;
+    std::vector<std::map<int,int> > m_accum;  // column=rho, row=theta
+    int distCenter;
+    int lastDist;
+    int dist;
+    int stepDir;
+    int cell;
+    int m_rowLength;
+    int m_numAccumulated;
+    int m_rhoResolutionFactor;
+    int m_numAngleCells;
+    std::vector<double> m_cosTable;
+    std::vector<double> m_sinTable;
+    bool DoAddPoint(int x, int y);
+  }; // class HoughTransform  
+
   class HoughLineService {
     
   public:
@@ -31,52 +75,7 @@ namespace cluster {
          
     size_t Transform(art::PtrVector<recob::Cluster>& clusIn,
      	             std::vector<recob::Cluster>& ccol);
-     
-    class HoughTransform {
-    public:
-
-      HoughTransform();
-      ~HoughTransform();
-
-  
-      void Init(int dx, int dy, int rhoresfact, int numACells);
-      bool AddPoint(int x, int y);
-      int GetCell(int row, int col) { return m_accum[row][col]; }
-      void SetCell(int row, int col, int value) { m_accum[row][col] = value; }
-      void IncrementCell(int row, int col)      { m_accum[row][col]++;}
-      void GetAccumSize(int &numRows, int &numCols) 
-      { 
-	numRows = m_accum.size();
-	numCols  = m_rowLength;
-      }
-      int NumAccumulated() { return m_numAccumulated; }
-      void GetEquation(double row, double col, double &rho, double &theta)
-      {
-	theta = (TMath::Pi()*row)/m_numAngleCells;
-	rho   = (col - (m_rowLength/2.))/m_rhoResolutionFactor;
-      }
-      int GetMax(int & xmax, int & ymax);
-
-      
-    private:
-   
-      
-      int m_dx, m_dy;
-      std::vector<std::map<int,int> > m_accum;  // column=rho, row=theta
-      int distCenter;
-      int lastDist;
-      int dist;
-      int stepDir;
-      int cell;
-      int m_rowLength;
-      int m_numAccumulated;
-      int m_rhoResolutionFactor;
-      int m_numAngleCells;
-      std::vector<double> m_cosTable;
-      std::vector<double> m_sinTable;
-      bool DoAddPoint(int x, int y);
-    }; // class HoughTransform  
-     
+          
   private:
   
     void HLSSaveBMPFile(char const*, unsigned char*, int, int);
