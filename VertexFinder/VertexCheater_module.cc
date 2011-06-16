e0decb9b949446839a109669b766f8a6a75ae6fb
Brian Rebel <brebel@fnal.gov>
2011-06-16 21:45:48 +0000

    move cheater reco modules into appropriate reco packages
    Former-commit-id: b0d6dde8594f8d1b888aef872e052a488c44b9f1

---
+++ VertexFinder/VertexCheater_module.cc
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: VertexCheater_module.cc Exp $
+//
+// VertexCheater module
+//
+// brebel@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+// LArSoft includes
+#include "MCCheater/VertexCheater.h"
+
+// Framework includes
+#include "art/Framework/Core/ModuleMacros.h"
+
+namespace cheat{
+
+  DEFINE_ART_MODULE(VertexCheater);
+
+}
