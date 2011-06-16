8e815e87245fa962c48e8e4641e9576f851c45b8
Brian Rebel <brebel@fnal.gov>
2011-06-16 21:27:47 +0000

    move cheater reco modules into appropriate reco packages
    Former-commit-id: 0152f777f4bd79d8138ecbbe0e3635c3259b091b

---
+++ ClusterFinder/ClusterCheater.h
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////
+/// \file    ClusterCheater.h
+/// \brief   make clusters using MC truth information
+/// \author  brebel@fnal.gov
+/// \version $Id: GeometryTest.h,v 1.1 2011/02/17 01:45:48 brebel Exp $
+///////////////////////////////////////////////////////////////////////
+#ifndef CHEAT_CLUSTERCHEATER_H
+#define CHEAT_CLUSTERCHEATER_H
+#include <string>
+
+#include "art/Framework/Core/EDProducer.h"
+
+namespace cheat {
+  class ClusterCheater : public art::EDProducer {
+  public:
+    explicit ClusterCheater(fhicl::ParameterSet const& pset);
+    virtual ~ClusterCheater();
+
+    void produce(art::Event& evt);
+
+    void reconfigure(fhicl::ParameterSet pset);
+
+ private:
+
+    std::string fHitModuleLabel;    ///< label for module creating recob::Hit objects	   
+    std::string fG4ModuleLabel;     ///< label for module running G4 and making particles, etc
+    std::string fDetSimModuleLabel; ///< label for module creating sim::SimChannel objects
+
+  };
+}
+#endif
