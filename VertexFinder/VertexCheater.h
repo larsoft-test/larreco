a99e6687d5a3f0e200f9f128fb5d6a44a23a4593
Brian Rebel <brebel@fnal.gov>
2011-06-16 21:44:35 +0000

    move cheater reco modules into appropriate reco packages
    Former-commit-id: 57262e234217029422d11f0025fa10eef154e951

---
+++ VertexFinder/VertexCheater.h
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////
+/// \file    VertexCheater.h
+/// \brief   make vertices using MC truth information
+/// \author  brebel@fnal.gov
+/// \version $Id: GeometryTest.h,v 1.1 2011/02/17 01:45:48 brebel Exp $
+///////////////////////////////////////////////////////////////////////
+#ifndef CHEAT_VERTEXCHEATER_H
+#define CHEAT_VERTEXCHEATER_H
+#include <string>
+
+#include "art/Framework/Core/EDProducer.h"
+
+namespace cheat {
+  class VertexCheater : public art::EDProducer {
+  public:
+    explicit VertexCheater(fhicl::ParameterSet const& pset);
+    virtual ~VertexCheater();
+
+    void produce(art::Event& evt);
+
+    void reconfigure(fhicl::ParameterSet pset);
+
+ private:
+
+    std::string fCheatedTrackLabel;  ///< label for module creating recob::Prong objects
+    std::string fCheatedShowerLabel; ///< label for module creating recob::Prong objects
+    std::string fG4ModuleLabel;      ///< label for module running G4 and making particles, etc
+    std::string fDetSimModuleLabel;  ///< label for module creating sim::SimChannel objects
+
+  };
+}
+#endif
