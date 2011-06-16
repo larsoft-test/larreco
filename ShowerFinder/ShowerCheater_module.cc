feef0461da5c0861e9c9668bff915851546c77aa
Brian Rebel <brebel@fnal.gov>
2011-06-16 21:42:58 +0000

    move cheater reco modules into appropriate reco packages
    Former-commit-id: 92afe7ecaee22eb4a4b70b625e80cb0a047d6667

---
+++ ShowerFinder/ShowerCheater_module.cc
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: ShowerCheater_module.cc Exp $
+//
+// ShowerCheater module
+//
+// brebel@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+// LArSoft includes
+#include "MCCheater/ShowerCheater.h"
+
+// Framework includes
+#include "art/Framework/Core/ModuleMacros.h"
+
+namespace cheat{
+
+  DEFINE_ART_MODULE(ShowerCheater);
+
+}
