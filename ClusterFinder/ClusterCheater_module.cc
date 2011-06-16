936451fc7093af0c89fedd83ca5535011e533f8b
Brian Rebel <brebel@fnal.gov>
2011-06-16 21:34:20 +0000

    move cheater reco modules into appropriate reco packages
    Former-commit-id: f5dc880656dd47e195699c4e5bc4e79bf46cc305

---
+++ ClusterFinder/ClusterCheater_module.cc
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: ClusterCheater_module.cc Exp $
+//
+// ClusterCheater module
+//
+// brebel@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+// LArSoft includes
+#include "MCCheater/ClusterCheater.h"
+
+// Framework includes
+#include "art/Framework/Core/ModuleMacros.h"
+
+namespace cheat{
+
+  DEFINE_ART_MODULE(ClusterCheater);
+
+}
