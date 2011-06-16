e7fa0647461ed5800f3bd4e15bb6398f5fbc05be
Brian Rebel <brebel@fnal.gov>
2011-06-16 18:38:08 +0000

    a few more tweaks for the geometry change from yesterday, remove unused variables
    Former-commit-id: 2f889f01908a23753e51aba94db7d419c37f5eee

--- VertexFinder/HarrisVertexFinder.cxx
+++ VertexFinder/HarrisVertexFinder.cxx
@@ -145,210 +145,192 @@ void vertex::HarrisVertexFinder::produce(art::Event& evt)
     }
   }
   
-  unsigned int channel,plane,tpc,wire,wire2;
-  for(unsigned int p = 0; p < geom->Nplanes(); p++) 
-    {
-      art::PtrVector<recob::Hit> vHits;
-      art::PtrVectorItr<recob::Cluster> clusterIter = clusIn.begin();
-      hit.clear();
-      cHits.clear();      
-      while(clusterIter!= clusIn.end() ) {
-	cHits = (*clusterIter)->Hits(p);
-	if(cHits.size() > 0)
-	  //hit.insert(hit.end(),cHits.begin(),cHits.end());
-      for(unsigned int i = 0; i < cHits.size(); i++)
-      hit.push_back(cHits[i]);
+  unsigned int channel,plane,wire,wire2,tpc;
+  for(unsigned int p = 0; p < geom->Nplanes(); p++) {
+    art::PtrVector<recob::Hit> vHits;
+    art::PtrVectorItr<recob::Cluster> clusterIter = clusIn.begin();
+    hit.clear();
+    cHits.clear();      
+    while(clusterIter!= clusIn.end() ) {
+      cHits = (*clusterIter)->Hits(p);
+      if(cHits.size() > 0)
+	//hit.insert(hit.end(),cHits.begin(),cHits.end());
+	for(unsigned int i = 0; i < cHits.size(); i++)
+	  hit.push_back(cHits[i]);
       
-	clusterIter++;  
-      } 
-      if(hit.size() == 0) 
-        continue;
-
-      numberwires=geom->Nwires(0);
-      numbertimesamples=hit[0]->Wire()->fSignal.size();
-      double MatrixAsum[numberwires][fTimeBins];
-      double MatrixBsum[numberwires][fTimeBins];
-      double hit_map[numberwires][fTimeBins];//the map of hits 
-      int hit_loc[numberwires][fTimeBins];//the index of the hit that corresponds to the potential corner
-      double Cornerness[numberwires][fTimeBins];//the "weight" of a corner
-  
-      for(unsigned int wi=0;wi < numberwires; wi++)
-	for(int timebin=0;timebin < fTimeBins; timebin++)
-	  {
-	    hit_map[wi][timebin]=0.;
-	    hit_loc[wi][timebin]=-1;
-	    Cornerness[wi][timebin]=0.;
-	    MatrixAsum[wi][timebin]=0.;
-	    MatrixBsum[wi][timebin]=0.;
-	  }
+      clusterIter++;  
+    } 
+    if(hit.size() == 0) 
+      continue;
+    
+    numberwires=geom->Nwires(0);
+    numbertimesamples=hit[0]->Wire()->fSignal.size();
+    double MatrixAsum[numberwires][fTimeBins];
+    double MatrixBsum[numberwires][fTimeBins];
+    double hit_map[numberwires][fTimeBins];//the map of hits 
+    int hit_loc[numberwires][fTimeBins];//the index of the hit that corresponds to the potential corner
+    double Cornerness[numberwires][fTimeBins];//the "weight" of a corner
+    
+    for(unsigned int wi=0;wi < numberwires; wi++)
+      for(int timebin=0;timebin < fTimeBins; timebin++){
+	hit_map[wi][timebin]=0.;
+	hit_loc[wi][timebin]=-1;
+	Cornerness[wi][timebin]=0.;
+	MatrixAsum[wi][timebin]=0.;
+	MatrixBsum[wi][timebin]=0.;
+      }
              
-      for(unsigned int i=0;i < hit.size(); i++)
-	{
-	  channel=hit[i]->Wire()->RawDigit()->Channel();
-	  geom->ChannelToWire(channel,tpc,plane,wire);
-	  //pixelization using a Gaussian
-	  for(int j=0;j <= (int)(hit[i]->EndTime()-hit[i]->StartTime()+.5); j++)    
-	    hit_map[wire][(int)((hit[i]->StartTime()+j)*(fTimeBins/numbertimesamples)+.5)]+=Gaussian((int)(j-((hit[i]->EndTime()-hit[i]->StartTime())/2.)+.5),0,hit[i]->EndTime()-hit[i]->StartTime());      
-	}
+    for(unsigned int i=0;i < hit.size(); i++){
+      channel=hit[i]->Wire()->RawDigit()->Channel();
+      geom->ChannelToWire(channel,tpc,plane,wire);
+      //pixelization using a Gaussian
+      for(int j=0;j <= (int)(hit[i]->EndTime()-hit[i]->StartTime()+.5); j++)    
+	hit_map[wire][(int)((hit[i]->StartTime()+j)*(fTimeBins/numbertimesamples)+.5)]+=Gaussian((int)(j-((hit[i]->EndTime()-hit[i]->StartTime())/2.)+.5),0,hit[i]->EndTime()-hit[i]->StartTime());      
+    }
 	
-      ////Gaussian derivative convolution  
-      for(unsigned int wire=1;wire < numberwires-1; wire++)
-	for(int timebin=1;timebin < fTimeBins-1; timebin++)
-	  {
-	    MatrixAsum[wire][timebin]=0.;
-	    MatrixBsum[wire][timebin]=0.;
-	    n=0;
-	    for(int i = -3; i <= 3; i++) 
-              {
-		windex=0;
-                for(int j = -3; j <= 3; j++) 
-		  {
-		    tindex=0;
-		    while(wire+i+windex<0)
-		      windex++;
-		    while(wire+i+windex>numberwires)
-		      windex--;
-		    while(timebin+j+tindex<0)
-		      tindex++;
-		    while(timebin+j+tindex>fTimeBins)
-		      tindex--;               
-		    MatrixAsum[wire][timebin]+=wx[n]*hit_map[wire+i+windex][timebin+j+tindex];  
-		    MatrixBsum[wire][timebin]+=wy[n]*hit_map[wire+i+windex][timebin+j+tindex]; 
-		    n++;
-		  }
-              }
+    ////Gaussian derivative convolution  
+    for(unsigned int wire=1;wire < numberwires-1; wire++)
+      for(int timebin=1;timebin < fTimeBins-1; timebin++){
+	MatrixAsum[wire][timebin]=0.;
+	MatrixBsum[wire][timebin]=0.;
+	n=0;
+	for(int i = -3; i <= 3; i++) {
+	  windex=0;
+	  for(int j = -3; j <= 3; j++) {
+	    tindex=0;
+	    while(wire+i+windex<0)
+	      windex++;
+	    while(wire+i+windex>numberwires)
+	      windex--;
+	    while(timebin+j+tindex<0)
+	      tindex++;
+	    while(timebin+j+tindex>fTimeBins)
+	      tindex--;               
+	    MatrixAsum[wire][timebin]+=wx[n]*hit_map[wire+i+windex][timebin+j+tindex];  
+	    MatrixBsum[wire][timebin]+=wy[n]*hit_map[wire+i+windex][timebin+j+tindex]; 
+	    n++;
 	  }
+	}
+      }
      
       //calculate the cornerness of each pixel while making sure not to fall off the hit map.
-      for(unsigned int wire=1;wire < numberwires-1; wire++)
-	for(int timebin=1;timebin < fTimeBins-1; timebin++)
-	  {     
-	    MatrixAAsum=0;
-	    MatrixBBsum=0;
-	    MatrixCCsum=0;
-	    //Gaussian smoothing convolution
-	    n=0;
-
-	    for(int i = -3; i <= 3; i++) 
-              {
-		windex=0;
-                for(int j = -3; j <= 3; j++) 
-		  {
-		    tindex=0;
-		    while(wire+i+windex<0)
-		      windex++;
-		    while(wire+i+windex>numberwires)
-		      windex--;
-		    while(timebin+j+tindex<0)
-		      tindex++;
-		    while(timebin+j+tindex>fTimeBins)
-		      tindex--;               
-		    MatrixAAsum+=w[n]*pow(MatrixAsum[wire+i][timebin+j],2);  
-		    MatrixBBsum+=w[n]*pow(MatrixBsum[wire+i][timebin+j],2);                   
-		    MatrixCCsum+=w[n]*MatrixAsum[wire+i][timebin+j]*MatrixBsum[wire+i][timebin+j]; 
-		    n++;
-		  }
-              }
-       
-	    if((MatrixAAsum+MatrixBBsum)>0)		
-	      Cornerness[wire][timebin]=(MatrixAAsum*MatrixBBsum-pow(MatrixCCsum,2))/(MatrixAAsum+MatrixBBsum);
-	    else
-	      Cornerness[wire][timebin]=0;
-        
-	    if(Cornerness[wire][timebin]>0)
-	      {	  
-		for(unsigned int i=0;i < hit.size(); i++)
-		  {
-		    channel=hit[i]->Wire()->RawDigit()->Channel();
-		    geom->ChannelToWire(channel,tpc,plane,wire2);	 
-		    //make sure the vertex candidate coincides with an actual hit.
-		    if(wire==wire2 && hit[i]->StartTime()<timebin*(numbertimesamples/fTimeBins) 
-		       && hit[i]->EndTime()>timebin*(numbertimesamples/fTimeBins))
-		      { 	       
-			//this index keeps track of the hit number
-			hit_loc[wire][timebin]=i;
-			Cornerness2.push_back(Cornerness[wire][timebin]);
-			break;
-		      } 	        
-		  }	     
-	      }	    
-	  }      
-      std::sort(Cornerness2.rbegin(),Cornerness2.rend());
-
-      for(int vertexnum=0;vertexnum<fMaxCorners;vertexnum++)
-	{
-	  flag=0;
-	  for(unsigned int wire=0;wire < numberwires && flag==0; wire++)
-	    for(int timebin=0;timebin < fTimeBins && flag==0; timebin++)
-	      {    
-		if(Cornerness2.size()>(unsigned int)vertexnum)
-		  if(Cornerness[wire][timebin]==Cornerness2[vertexnum] 
-		     && Cornerness[wire][timebin]>0. 
-		     && hit_loc[wire][timebin]>-1)
-		    {
-		      flag++;      
-		      //thresholding
-		      if(Cornerness2.size())
-			if(Cornerness[wire][timebin]<(fThreshold*Cornerness2[0]))
-			  vertexnum=fMaxCorners;
-		      vHits.push_back(hit[hit_loc[wire][timebin]]);
-		      recob::EndPoint2D vertex(vHits);
-		      vertex.SetWireNum(wire);
-		      vertex.SetDriftTime(hit[hit_loc[wire][timebin]]->PeakTime());
-		      //weak vertices are given vertex id=1
-		      vertex.SetID(1);
-		      vertex.SetStrength(Cornerness[wire][timebin]);	  
-		      vtxcol->push_back(vertex);
-		      vHits.clear();
-		      // non-maximal suppression on a square window. The wire coordinate units are 
-		      // converted to time ticks so that the window is truly square. 
-		      // Note that there are 1/0.0743=13.46 time samples per 4.0 mm (wire pitch in ArgoNeuT), 
-		      // assuming a 1.5 mm/us drift velocity for a 500 V/cm E-field 
-		      for(unsigned int wireout=wire-(int)((fWindow*(numbertimesamples/fTimeBins)*.0743)+.5);
-			  wireout <= wire+(int)((fWindow*(numbertimesamples/fTimeBins)*.0743)+.5) ; wireout++)
-			for(int timebinout=timebin-fWindow;timebinout <= timebin+fWindow; timebinout++)
-			  if(sqrt(pow(wire-wireout,2)+pow(timebin-timebinout,2))<fWindow)//circular window 
-			    Cornerness[wireout][timebinout]=0;	  
-		    }     
-	      }
+    for(unsigned int wire=1;wire < numberwires-1; wire++)
+      for(int timebin=1;timebin < fTimeBins-1; timebin++){     
+	MatrixAAsum=0;
+	MatrixBBsum=0;
+	MatrixCCsum=0;
+	//Gaussian smoothing convolution
+	n=0;
+	
+	for(int i = -3; i <= 3; i++) {
+	  windex=0;
+	  for(int j = -3; j <= 3; j++) {
+	    tindex=0;
+	    while(wire+i+windex<0)
+	      windex++;
+	    while(wire+i+windex>numberwires)
+	      windex--;
+	    while(timebin+j+tindex<0)
+	      tindex++;
+	    while(timebin+j+tindex>fTimeBins)
+	      tindex--;               
+	    MatrixAAsum+=w[n]*pow(MatrixAsum[wire+i][timebin+j],2);  
+	    MatrixBBsum+=w[n]*pow(MatrixBsum[wire+i][timebin+j],2);                   
+	    MatrixCCsum+=w[n]*MatrixAsum[wire+i][timebin+j]*MatrixBsum[wire+i][timebin+j]; 
+	    n++;
+	  }
 	}
-      Cornerness2.clear();
-      hit.clear();
-      if(clusterIter!=clusIn.end()) clusterIter++;
-
-      if(p==(unsigned int)fSaveVertexMap)
-	{ 
-	  unsigned char *outPix = new unsigned char [fTimeBins*numberwires];
-	  //finds the maximum cell in the map for image scaling
-	  int cell, pix=0, maxCell=0;
-	  int xmaxx, ymaxx;
-	  for (int y=0; y<fTimeBins; y++)
-	    for (unsigned int x=0; x<numberwires; x++)
-	      {
-		cell = (int)(hit_map[x][y]*1000);
-		if (cell > maxCell){
-		  maxCell = cell;
-		  xmaxx=x;
-		  ymaxx=y;
-		}
-	      }
-       
-	  for (unsigned int y=0; y<(unsigned int)fTimeBins; y++)
-	    for (unsigned int x=0; x<numberwires; x++)
-	      { 
-		//scales the pixel weights based on the maximum cell value     
-		if(maxCell>0)
-		  pix = (int)((1500000*hit_map[x][y])/maxCell);
-		outPix[y*numberwires + x] = pix;
-	      }
-	  SaveBMPFile("harrisvertexmap.bmp", outPix, numberwires, fTimeBins);
-	  delete [] outPix;
-	}   
-   }
-    std::cout<<"Size of vtxcol= "<<vtxcol->size()<<std::endl;
-    fNoVertices->Fill(evt.id().event(),vtxcol->size());
-
-evt.put(vtxcol);   
+	
+	if((MatrixAAsum+MatrixBBsum)>0)		
+	  Cornerness[wire][timebin]=(MatrixAAsum*MatrixBBsum-pow(MatrixCCsum,2))/(MatrixAAsum+MatrixBBsum);
+	else
+	  Cornerness[wire][timebin]=0;
+        
+	if(Cornerness[wire][timebin]>0){	  
+	  for(unsigned int i=0;i < hit.size(); i++){
+	    channel=hit[i]->Wire()->RawDigit()->Channel();
+	    geom->ChannelToWire(channel,tpc,plane,wire2);	 
+	    //make sure the vertex candidate coincides with an actual hit.
+	    if(wire==wire2 && hit[i]->StartTime()<timebin*(numbertimesamples/fTimeBins) 
+	       && hit[i]->EndTime()>timebin*(numbertimesamples/fTimeBins)){ 	       
+	      //this index keeps track of the hit number
+	      hit_loc[wire][timebin]=i;
+	      Cornerness2.push_back(Cornerness[wire][timebin]);
+	      break;
+	    } 	        
+	  }	     
+	}	    
+      }      
+    std::sort(Cornerness2.rbegin(),Cornerness2.rend());
+    
+    for(int vertexnum=0;vertexnum<fMaxCorners;vertexnum++){
+      flag=0;
+      for(unsigned int wire=0;wire < numberwires && flag==0; wire++)
+	for(int timebin=0;timebin < fTimeBins && flag==0; timebin++){    
+	  if(Cornerness2.size()>(unsigned int)vertexnum)
+	    if(Cornerness[wire][timebin]==Cornerness2[vertexnum] 
+	       && Cornerness[wire][timebin]>0. 
+	       && hit_loc[wire][timebin]>-1){
+	      flag++;      
+	      //thresholding
+	      if(Cornerness2.size())
+		if(Cornerness[wire][timebin]<(fThreshold*Cornerness2[0]))
+		  vertexnum=fMaxCorners;
+	      vHits.push_back(hit[hit_loc[wire][timebin]]);
+	      recob::EndPoint2D vertex(vHits);
+	      vertex.SetWireNum(wire);
+	      vertex.SetDriftTime(hit[hit_loc[wire][timebin]]->PeakTime());
+	      //weak vertices are given vertex id=1
+	      vertex.SetID(1);
+	      vertex.SetStrength(Cornerness[wire][timebin]);	  
+	      vtxcol->push_back(vertex);
+	      vHits.clear();
+	      // non-maximal suppression on a square window. The wire coordinate units are 
+	      // converted to time ticks so that the window is truly square. 
+	      // Note that there are 1/0.0743=13.46 time samples per 4.0 mm (wire pitch in ArgoNeuT), 
+	      // assuming a 1.5 mm/us drift velocity for a 500 V/cm E-field 
+	      for(unsigned int wireout=wire-(int)((fWindow*(numbertimesamples/fTimeBins)*.0743)+.5);
+		  wireout <= wire+(int)((fWindow*(numbertimesamples/fTimeBins)*.0743)+.5) ; wireout++)
+		for(int timebinout=timebin-fWindow;timebinout <= timebin+fWindow; timebinout++)
+		  if(sqrt(pow(wire-wireout,2)+pow(timebin-timebinout,2))<fWindow)//circular window 
+		    Cornerness[wireout][timebinout]=0;	  
+	    }     
+	}
+    }
+    Cornerness2.clear();
+    hit.clear();
+    if(clusterIter!=clusIn.end()) clusterIter++;
+    
+    if(p==(unsigned int)fSaveVertexMap){ 
+      unsigned char *outPix = new unsigned char [fTimeBins*numberwires];
+      //finds the maximum cell in the map for image scaling
+      int cell, pix=0, maxCell=0;
+      int xmaxx, ymaxx;
+      for (int y=0; y<fTimeBins; y++)
+	for (unsigned int x=0; x<numberwires; x++){
+	  cell = (int)(hit_map[x][y]*1000);
+	  if (cell > maxCell){
+	    maxCell = cell;
+	    xmaxx=x;
+	    ymaxx=y;
+	  }
+	}
+      
+      for (unsigned int y=0; y<(unsigned int)fTimeBins; y++)
+	for (unsigned int x=0; x<numberwires; x++){ 
+	  //scales the pixel weights based on the maximum cell value     
+	  if(maxCell>0)
+	    pix = (int)((1500000*hit_map[x][y])/maxCell);
+	  outPix[y*numberwires + x] = pix;
+	}
+      SaveBMPFile("harrisvertexmap.bmp", outPix, numberwires, fTimeBins);
+      delete [] outPix;
+    }   
+  }
+  std::cout<<"Size of vtxcol= "<<vtxcol->size()<<std::endl;
+  fNoVertices->Fill(evt.id().event(),vtxcol->size());
+  
+  evt.put(vtxcol);   
 }
 
 //-----------------------------------------------------------------------------
--- VertexFinder/VertexMatch.cxx
+++ VertexFinder/VertexMatch.cxx
@@ -108,68 +108,70 @@ void vertex::VertexMatch::produce(art::Event& evt)
       art::Ptr<recob::Cluster> cluster(houghListHandle, ii);
       houghIn.push_back(cluster);
     }
-  unsigned int channel,tpc,plane,wire;
+  unsigned int channel,plane,wire,tpc;
   double slope,intercept,distance;
   double starttime, endtime;
   int startwire, endwire;
   double strength; //the strength of a strong vertex
-  for(unsigned int p = 0; p < geom->Nplanes(); p++) {
-    //create the vector of vertex hits 
-    art::PtrVectorItr<recob::EndPoint2D> vertexIter = vertIn.begin();
-    art::PtrVectorItr<recob::Cluster> houghIter = houghIn.begin();
-    while(vertexIter!= vertIn.end() ) 
-      {
-	// vHits = (*vertexIter)->Hits(p,-1);
-	//      if(vHits.size() > 0){
-	//  	 vertexhit.insert(vertexhit.end(),vHits.begin(),vHits.end());
-	//  	 weakvertexstrength.push_back((*vertexIter)->Strength());
-	//  	 }
-	vHits = (*vertexIter)->Hits(p);
-	if(vHits.size() > 0)
-	  {
-	    art::PtrVectorItr<recob::Hit> vertexhitIter = vHits.begin();
-	    while (vertexhitIter!=vHits.end())
-	      {
-		vertexhit.push_back((*vertexhitIter));
-		weakvertexstrength.push_back((*vertexIter)->Strength());
-		vertexhitIter++;
-	      }       
-	  }
-	vertexIter++;
 
-      } 
-    
+  for(unsigned int t = 0; t < geom->NTPC(); ++t){
+    for(unsigned int p = 0; p < geom->Nplanes(t); p++) {
+      //create the vector of vertex hits 
+      art::PtrVectorItr<recob::EndPoint2D> vertexIter = vertIn.begin();
+      art::PtrVectorItr<recob::Cluster> houghIter = houghIn.begin();
+      while(vertexIter!= vertIn.end() ) 
+	{
+	  // vHits = (*vertexIter)->Hits(p,-1);
+	  //      if(vHits.size() > 0){
+	  //  	 vertexhit.insert(vertexhit.end(),vHits.begin(),vHits.end());
+	  //  	 weakvertexstrength.push_back((*vertexIter)->Strength());
+	  //  	 }
+	  vHits = (*vertexIter)->Hits(p);
+	  if(vHits.size() > 0)
+	    {
+	      art::PtrVectorItr<recob::Hit> vertexhitIter = vHits.begin();
+	      while (vertexhitIter!=vHits.end())
+		{
+		  vertexhit.push_back((*vertexhitIter));
+		  weakvertexstrength.push_back((*vertexIter)->Strength());
+		  vertexhitIter++;
+		}       
+	    }
+	  vertexIter++;
+
+	} 
       
-    if(vHits.size() == 0)
-      continue;
-    
-    vHits.clear();
-    //loop over vector of hough lines and find the vertex hits that are associated with the hough line(s)
-    houghIter = houghIn.begin();  
-    while(houghIter!= houghIn.end()) 
-      { 
-	houghhit.clear();
-	hHits.clear();
-	plane=-1; 
-	distance=-1.;
-	//create vector of hits associated with hough line
-	hHits = (*houghIter)->Hits(p);
+      
+      if(vHits.size() == 0)
+	continue;
+      
+      vHits.clear();
+      //loop over vector of hough lines and find the vertex hits that are associated with the hough line(s)
+      houghIter = houghIn.begin();  
+      while(houghIter!= houghIn.end()) 
+	{ 
+	  houghhit.clear();
+	  hHits.clear();
+	  plane=-1; 
+	  distance=-1.;
+	  //create vector of hits associated with hough line
+	  hHits = (*houghIter)->Hits(p);
 	
-        if(hHits.size() > 0)
-	  {
-
- 	    art::PtrVectorItr<recob::Hit> hitIter = hHits.begin();
-	    while (hitIter!=hHits.end())
-	      {
-		houghhit.push_back((*hitIter));
-		hitIter++;
-	      }  
-	  }
-        if(houghhit.size()){
-	  channel=houghhit[0]->Wire()->RawDigit()->Channel();
-	  geom->ChannelToWire(channel,tpc,plane,wire);
+	  if(hHits.size() > 0)
+	    {
+	      
+	      art::PtrVectorItr<recob::Hit> hitIter = hHits.begin();
+	      while (hitIter!=hHits.end())
+		{
+		  houghhit.push_back((*hitIter));
+		  hitIter++;
+		}  
+	    }
+	  if(houghhit.size()){
+	    channel=houghhit[0]->Wire()->RawDigit()->Channel();
+	    geom->ChannelToWire(channel,tpc,plane,wire);
 	}
-	if(p==plane)
+	if(p==plane && t == tpc)
 	  {
 	    slope=(*houghIter)->dTdW();
 	    intercept=(*houghIter)->StartPos()[1] - slope*(*houghIter)->StartPos()[0];
@@ -190,94 +192,95 @@ void vertex::VertexMatch::produce(art::Event& evt)
 		//(within a window) of a Hough line. A strong vertex matches up with at least two Hough lines. 
 		if(((TMath::Abs((int)(wire-startwire))<fMaxDistance*.0743)
 		    ||(TMath::Abs((int)(wire-endwire))<fMaxDistance*.0743)
-		   )
+		    )
 		   &&((TMath::Abs(vertexhit[i]->PeakTime()-starttime)<fMaxDistance)
 		      ||(TMath::Abs(vertexhit[i]->PeakTime()-endtime)<fMaxDistance)
-		     ))          		  distance=(TMath::Abs(vertexhit[i]->PeakTime()-slope*(double)wire-intercept)/(sqrt(pow(.0743*slope,2)+1))); 
-
+		      ))          		  distance=(TMath::Abs(vertexhit[i]->PeakTime()-slope*(double)wire-intercept)/(sqrt(pow(.0743*slope,2)+1))); 
+		
 		if(distance<(fMaxDistance+((vertexhit[i]->EndTime()-vertexhit[i]->StartTime())/2.))&&distance>-1)
 		  matchedvertex.push_back(std::pair<art::Ptr<recob::Hit>,double>(vertexhit[i], weakvertexstrength[i]*sqrt(pow(TMath::Abs(endwire-startwire)*.0743,2)+pow(TMath::Abs(endtime-starttime),2))));
 		//ala strongestvertex.push_back(std::pair<art::PtrVector<recob::Hit>,double>(matchedvertex[i].first,strength));
 	      }
 	  }
-  
+	
 	if(vertexhit.size() == 0 || houghhit.size() == 0) 
 	  {
 	    houghIter++;
 	    continue;
 	  }
-   
+	
 	if(vertexIter!=vertIn.end()) vertexIter++;
 	if(houghIter!=houghIn.end()) houghIter++;    
-      }
-   
-    //sort matchedvertex vector to make it easy to find duplicate entries (strong vertices)
-    std::sort(matchedvertex.rbegin(), matchedvertex.rend(),sort_pred);
-
-    // the "strength" of a strong vertex is defined as 
-    // (HarrisVertexStrength*LengthofHoughLine)_1+(HarrisVertexStrength*LengthofHoughLine)_2+...
-    // ...+(HarrisVertexStrength*LengthofHoughLine)_n, where n is the number of vertices 
-    // associated with a Hough Line
-    for(unsigned int i=0;i < matchedvertex.size(); i++) 
-      {
-	strength=matchedvertex[i].second;
-  
-	for(unsigned int n=1;n < matchedvertex.size() && i>=n; n++)
-	  if(matchedvertex[i].first==matchedvertex[i-n].first)	
-	    strength+=matchedvertex[i-n].second;
-  
-	strongvertexstrength.push_back(strength);
-	//make sure there is more than one Hough Line associated with the vertex 
-          
-	if(strength>matchedvertex[i].second)
-	  strongestvertex.push_back(std::pair<art::Ptr<recob::Hit>,double>(matchedvertex[i].first,strength));
-      }
-  
-  
-    //sort the strength of the strong vertices to find the strongest vertex
-    std::sort(strongestvertex.rbegin(), strongestvertex.rend(),sort_pred2);
-    for(unsigned int i=0;i < matchedvertex.size(); i++)
-      {      
-	// I think this is grabbing first item in pair, itself a pointer then grabbing first 
-	// (.begin()) one of those. EC, 18-Oct-2010.
-	channel=(matchedvertex[i].first)->Wire()->RawDigit()->Channel();
-	geom->ChannelToWire(channel,tpc,plane,wire);
-
-	// strongvertex, despite name, is a hit vector.
-	strongvertex.push_back(matchedvertex[i].first);
-	recob::EndPoint2D vertex(strongvertex);      
-	vertex.SetWireNum(wire);
-	vertex.SetStrength(strongvertexstrength[i]);
-	//vertex->SetDriftTime(matchedvertex[i].first->PeakTime());
-	vertex.SetDriftTime((matchedvertex[i].first)->PeakTime());
-
-	//find the strong vertices, those vertices that have been associated with more than one hough line  
-	if(i>0)
+	}
+      
+      //sort matchedvertex vector to make it easy to find duplicate entries (strong vertices)
+      std::sort(matchedvertex.rbegin(), matchedvertex.rend(),sort_pred);
+      
+      // the "strength" of a strong vertex is defined as 
+      // (HarrisVertexStrength*LengthofHoughLine)_1+(HarrisVertexStrength*LengthofHoughLine)_2+...
+      // ...+(HarrisVertexStrength*LengthofHoughLine)_n, where n is the number of vertices 
+      // associated with a Hough Line
+      for(unsigned int i=0;i < matchedvertex.size(); i++) 
 	{
-	if(matchedvertex[i].first==matchedvertex[i-1].first)	
-	  {
-	    if(strongvertex[0]==(strongestvertex[0].first)&&strongestvertex.size()>0)
-	      vertex.SetID(4);//the strongest strong vertex is given a vertex id=4
-	    else
-	      vertex.SetID(3);//strong vertices are given vertex id=3
-	  } 
-	 } 
-	else
-	  {
-	    vertex.SetID(2);//weak vertices that have been associated with an endpoint of a single Hough line are given vertex id=2
-	  } 
-	mvertexcol->push_back(vertex);
-	strongvertex.clear();
-
-      }
-
-    strongestvertex.clear();
-    matchedvertex.clear();
-    vertexhit.clear();  
-  }
-
+	  strength=matchedvertex[i].second;
+	  
+	  for(unsigned int n=1;n < matchedvertex.size() && i>=n; n++)
+	    if(matchedvertex[i].first==matchedvertex[i-n].first)	
+	      strength+=matchedvertex[i-n].second;
+	  
+	  strongvertexstrength.push_back(strength);
+	  //make sure there is more than one Hough Line associated with the vertex 
+          
+	  if(strength>matchedvertex[i].second)
+	    strongestvertex.push_back(std::pair<art::Ptr<recob::Hit>,double>(matchedvertex[i].first,strength));
+	}
+      
+      
+      //sort the strength of the strong vertices to find the strongest vertex
+      std::sort(strongestvertex.rbegin(), strongestvertex.rend(),sort_pred2);
+      for(unsigned int i=0;i < matchedvertex.size(); i++)
+	{      
+	  // I think this is grabbing first item in pair, itself a pointer then grabbing first 
+	  // (.begin()) one of those. EC, 18-Oct-2010.
+	  channel=(matchedvertex[i].first)->Wire()->RawDigit()->Channel();
+	  geom->ChannelToWire(channel,tpc,plane,wire);
+	  
+	  // strongvertex, despite name, is a hit vector.
+	  strongvertex.push_back(matchedvertex[i].first);
+	  recob::EndPoint2D vertex(strongvertex);      
+	  vertex.SetWireNum(wire);
+	  vertex.SetStrength(strongvertexstrength[i]);
+	  //vertex->SetDriftTime(matchedvertex[i].first->PeakTime());
+	  vertex.SetDriftTime((matchedvertex[i].first)->PeakTime());
+	  
+	  //find the strong vertices, those vertices that have been associated with more than one hough line  
+	  if(i>0)
+	    {
+	      if(matchedvertex[i].first==matchedvertex[i-1].first)	
+		{
+		  if(strongvertex[0]==(strongestvertex[0].first)&&strongestvertex.size()>0)
+		    vertex.SetID(4);//the strongest strong vertex is given a vertex id=4
+		  else
+		    vertex.SetID(3);//strong vertices are given vertex id=3
+		} 
+	    } 
+	  else
+	    {
+	      vertex.SetID(2);//weak vertices that have been associated with an endpoint of a single Hough line are given vertex id=2
+	    } 
+	  mvertexcol->push_back(vertex);
+	  strongvertex.clear();
+	  
+	}
+      
+      strongestvertex.clear();
+      matchedvertex.clear();
+      vertexhit.clear();  
+    }
+  }//end loop over tpc
+    
   evt.put(mvertexcol);
-
 }
+  
 
 
