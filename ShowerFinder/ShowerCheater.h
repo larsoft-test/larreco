b4e73c2f5e47282062fe2a1b47558e5dd2f2b165
Brian Rebel <brebel@fnal.gov>
2011-06-16 21:43:40 +0000

    move cheater reco modules into appropriate reco packages
    Former-commit-id: 1e43354a49b33885d284736454b1b74db49d8832

---
+++ ShowerFinder/ShowerCheater.h
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////
+/// \file    ShowerCheater.h
+/// \brief   make prongs using MC truth information
+/// \author  brebel@fnal.gov
+/// \version $Id: GeometryTest.h,v 1.1 2011/02/17 01:45:48 brebel Exp $
+///////////////////////////////////////////////////////////////////////
+#ifndef CHEAT_SHOWERCHEATER_H
+#define CHEAT_SHOWERCHEATER_H
+#include <string>
+
+#include "art/Framework/Core/EDProducer.h"
+
+namespace cheat {
+  class ShowerCheater : public art::EDProducer {
+  public:
+    explicit ShowerCheater(fhicl::ParameterSet const& pset);
+    virtual ~ShowerCheater();
+
+    void produce(art::Event& evt);
+
+    void reconfigure(fhicl::ParameterSet pset);
+
+ private:
+
+    std::string fCheatedClusterLabel; ///< label for module creating recob::Cluster objects	   
+    std::string fG4ModuleLabel;       ///< label for module running G4 and making particles, etc
+    std::string fDetSimModuleLabel;   ///< label for module creating sim::SimChannel objects
+
+  };
+}
+#endif
