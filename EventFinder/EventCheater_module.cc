db9a972bc2596fd665e81eedec8362e0010ba27c
Brian Rebel <brebel@fnal.gov>
2011-07-05 19:16:57 +0000

    put algorithm to find an event by cheating into EventFinder
    Former-commit-id: 12d5ac9d1c08116b359384805a75e479044d6693

---
+++ EventFinder/EventCheater_module.cc
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////
+// $Id: EventCheater_module.cc Exp $
+//
+// EventCheater module
+//
+// brebel@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+// LArSoft includes
+#include "MCCheater/EventCheater.h"
+
+// Framework includes
+#include "art/Framework/Core/ModuleMacros.h"
+
+namespace cheat{
+
+  DEFINE_ART_MODULE(EventCheater);
+
+}
