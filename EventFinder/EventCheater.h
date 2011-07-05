9fcc5d502aaaca848c2890f5a096eaec24ec854f
Brian Rebel <brebel@fnal.gov>
2011-07-05 19:17:31 +0000

    put algorithm to find an event by cheating into EventFinder
    Former-commit-id: d519459afd33e8fd0eccd51a52717bd89ee69bad

---
+++ EventFinder/EventCheater.h
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////
+/// \file    EventCheater.h
+/// \brief   make events using MC truth information
+/// \author  brebel@fnal.gov
+/// \version $Id: GeometryTest.h,v 1.1 2011/02/17 01:45:48 brebel Exp $
+///////////////////////////////////////////////////////////////////////
+#ifndef CHEAT_VERTEXCHEATER_H
+#define CHEAT_VERTEXCHEATER_H
+#include <string>
+
+#include "art/Framework/Core/EDProducer.h"
+
+namespace cheat {
+  class EventCheater : public art::EDProducer {
+  public:
+    explicit EventCheater(fhicl::ParameterSet const& pset);
+    virtual ~EventCheater();
+
+    void produce(art::Event& evt);
+
+    void reconfigure(fhicl::ParameterSet pset);
+
+ private:
+
+    std::string fCheatedVertexLabel; ///< label for module creating recob::Vertex objects	   
+    std::string fG4ModuleLabel;      ///< label for module running G4 and making particles, etc
+    std::string fDetSimModuleLabel;  ///< label for module creating sim::SimChannel objects
+
+  };
+}
+#endif
