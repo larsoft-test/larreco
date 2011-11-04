d7d8c70047bf8e29cf84603267603fbaf514e3d1
Brian Rebel <brebel@fnal.gov>
2011-11-04 20:40:22 +0000

    keep backing out the SimListUtils as service
    Former-commit-id: 460a96047d9d434121dd73d4c41510ed8827bb03

--- ShowerFinder/ShowerCheater.cxx
+++ ShowerFinder/ShowerCheater.cxx
@@ -53,8 +53,7 @@ namespace shwf{
   {
 
     // grab the sim::ParticleList
-    art::ServiceHandle<sim::SimListUtils> slu;
-    sim::ParticleList plist = slu->GetParticleList();
+    sim::ParticleList plist = sim::SimListUtils::GetParticleList(evt, fG4ModuleLabel);
 
     // get the sim::SimChannels
     // get the sim::SimChannels as well
