9b0e9f1629812158e765fea9aed5dbd7e4d56e8e
Brian Rebel <brebel@fnal.gov>
2012-03-05 23:11:33 +0000

    make the association of hits to the super cluster work in LineMerger.  Previously only the hits from the seed cluster were added, now all hits from participating clusters in a super cluster should be added.  Also make the code easier to read.
    Former-commit-id: a517afdea45374b6ee8f0f72537ef48c9b1481d6

--- ClusterFinder/LineMerger.cxx
+++ ClusterFinder/LineMerger.cxx
@@ -70,12 +70,13 @@ namespace cluster{
     int nplanes = geo->Nplanes();
 
     //one PtrVector for each plane in the geometry
-    art::PtrVector<recob::Cluster> Cls[nplanes];
+    std::vector< art::PtrVector<recob::Cluster> > Cls(nplanes);
+
     //vector with indicators for whether a cluster has been merged already
-    std::vector<int> Cls_matches[nplanes];
+    std::vector< std::vector<int> > Cls_matches(nplanes);
 
     // loop over the input Clusters
-    for(unsigned int i = 0; i < clusterVecHandle->size(); ++i){
+    for(size_t i = 0; i < clusterVecHandle->size(); ++i){
       
       //get a art::Ptr to each Cluster
       art::Ptr<recob::Cluster> cl(clusterVecHandle, i);
@@ -95,68 +96,85 @@ namespace cluster{
 	break;
       default :
 	break;
-      }
-    }
+      }// end switch on view
+    }// end loop over input clusters
 
-     //////////////////////////////////////////////////////
-    // Make a std::auto_ptr<> for the thing you want to put into the event
-    // because that handles the memory management for you
-    //////////////////////////////////////////////////////
     std::auto_ptr<std::vector<recob::Cluster> >             SuperClusters(new std::vector<recob::Cluster>);
     std::auto_ptr< art::Assns<recob::Cluster, recob::Hit> > assn(new art::Assns<recob::Cluster, recob::Hit>);
 
-    for(int i = 0; i<nplanes; ++i){
-      int clustersfound = 0;//how many merged clusters found in each plane
-      int clsnum1 = 0;
-      for(art::PtrVector<recob::Cluster>::const_iterator clusIter1 = Cls[i].begin(); clusIter1!=Cls[i].end();++clusIter1){
-	art::Ptr<recob::Cluster> cl1 = *clusIter1;
-	if(Cls_matches[i][clsnum1]==1){
-	  clsnum1++;
+    for(int i = 0; i < nplanes; ++i){
+
+      int clustersfound = 0; // how many merged clusters found in each plane
+      int clsnum1       = 0;
+
+      for(size_t c = 0; c < Cls[i].size(); ++c){
+	if(Cls_matches[i][clsnum1] == 1){
+	  ++clsnum1;
 	  continue;
 	}
-	SuperClusters->push_back(*cl1); 
+	
+	// make a new cluster to put into the SuperClusters collection 
+	// because we want to be able to adjust it later
+	recob::Cluster cl1( *( Cls[i][c].get() ) ); 
 
-	// associate the hits with the cluster
-	// \todo get the hits in a better manner once they are no longer data members
-	art::PtrVector<recob::Hit> ptrvs = SuperClusters->back().Hits();
-	util::CreateAssn(*this, evt, *(SuperClusters.get()), ptrvs, *(assn.get()));
+	// find the hits associated with the current cluster
+	art::PtrVector<recob::Hit> ptrvs = util::FindManyP<recob::Hit>(Cls[i], evt, fClusterModuleLabel, c);
 
-	Cls_matches[i][clsnum1]=1; 
-	//SuperClusters->back().SetID(clustersfound);//IDs are sequential by plane, starting from 0
+	Cls_matches[i][clsnum1] = 1; 
 	++clustersfound;
 	
 	int clsnum2 = 0;
-	for(art::PtrVector<recob::Cluster>::const_iterator clusIter2 = Cls[i].begin(); clusIter2!=Cls[i].end();++clusIter2){
-	  art::Ptr<recob::Cluster> cl2 = *clusIter2;
-	  recob::Cluster SCl= SuperClusters->back(); //spitz moved this inside the for loop.
-	  if(Cls_matches[i][clsnum2]==1){
-	    clsnum2++;
+	for(size_t c2 = 0; c2 < Cls[i].size(); ++c2){
+
+	  recob::Cluster cl2( *(Cls[i][c2].get()) );
+
+	  if(Cls_matches[i][clsnum2] == 1){
+	    ++clsnum2;
 	    continue;
 	  }
+
+	  // find the hits associated with this second cluster
+	  art::PtrVector<recob::Hit> ptrvs2 = util::FindManyP<recob::Hit>(Cls[i], evt, fClusterModuleLabel, c2);
 	  
 	  // check that the slopes are the same
 	  // added 13.5 ticks/wirelength in ArgoNeuT. 
 	  // \todo need to make this detector agnostic
 	  // would be nice to have a LArProperties function that returns ticks/wire.
-	  bool sameSlope = SlopeCompatibility(SCl.dTdW()*(1./13.5),cl2->dTdW()*(1./13.5));  
+	  bool sameSlope = SlopeCompatibility(cl1.dTdW()*(1./13.5),
+					      cl2.dTdW()*(1./13.5));  
 	  
-	  //check that the endpoints fall within a circular window of each other 
-	  //spitz did this in place of intercept matching
-	  bool sameEndpoint = EndpointCompatibility(SCl.StartPos(),SCl.EndPos(),cl2->StartPos(),cl2->EndPos());
-
+	  // check that the endpoints fall within a circular window of each other 
+	  // done in place of intercept matching
+	  bool sameEndpoint = EndpointCompatibility(cl1.StartPos(), cl1.EndPos(),
+						    cl2.StartPos(), cl2.EndPos());
+	  
+	  // if the slopes and end points are the same, combine the clusters
+	  // note that after 1 combination cl1 is no longer what we started 
+	  // with
 	  if(sameSlope && sameEndpoint){
-	    SuperClusters->back() = SuperClusters->back() + *cl2;
-	    Cls_matches[i][clsnum2]=1;       
+	    cl1 = cl1 + cl2;
+	    Cls_matches[i][clsnum2] = 1;       
+
+	    // combine the hit collections
+	    for(size_t h = 0; h < ptrvs2.size(); ++h) ptrvs.push_back(ptrvs2[h]);
 	  }
-	  clsnum2++;
-	}
-	clsnum1++;
-      }
-    }
+	  
+	  ++clsnum2;
+	}// end loop over second cluster iterator
+
+	// now add the final version of cl1 to the collection of SuperClusters
+	// and create the association between the super cluster and the hits
+	SuperClusters->push_back(cl1);
+	util::CreateAssn(*this, evt, *(SuperClusters.get()), ptrvs, *(assn.get()));	
+
+	++clsnum1;
+
+      }// end loop over first cluster iterator
+    }// end loop over planes
 
     mf::LogVerbatim("Summary") << std::setfill('-') << std::setw(175) << "-" << std::setfill(' ');
     mf::LogVerbatim("Summary") << "LineMerger Summary:";
-    for(unsigned int i = 0; i<SuperClusters->size(); ++i) 
+    for(size_t i = 0; i < SuperClusters->size(); ++i) 
       mf::LogVerbatim("Summary") << SuperClusters->at(i);
 
     evt.put(SuperClusters);
@@ -170,32 +188,40 @@ namespace cluster{
   //checks the difference between angles of the two lines
   bool LineMerger::SlopeCompatibility(double slope1, double slope2)
   { 
-    double sl1=atan(slope1);
-    double sl2=atan(slope2);
-    bool comp = fabs(sl1-sl2)<fSlope ? true : false;//the units of fSlope are radians
+    double sl1 = atan(slope1);
+    double sl2 = atan(slope2);
+
+    //the units of fSlope are radians
+    bool comp  = fabs(sl1-sl2) < fSlope ? true : false;
 
     return comp;
   }
   //------------------------------------------------------------------------------------//
-  bool LineMerger::EndpointCompatibility(std::vector<double> sclstart, std::vector<double> sclend,std::vector<double> cl2start, std::vector<double> cl2end)
+  bool LineMerger::EndpointCompatibility(std::vector<double> sclstart, 
+					 std::vector<double> sclend,
+					 std::vector<double> cl2start, 
+					 std::vector<double> cl2end)
   { 
-   	  double sclstartwire=sclstart[0];
-	  double sclstarttime=sclstart[1];
-	  double sclendwire=sclend[0];
-	  double sclendtime=sclend[1];
-	  
-	  double cl2startwire=cl2start[0];
-	  double cl2starttime=cl2start[1];
-	  double cl2endwire=cl2end[0];
-	  double cl2endtime=cl2end[1];
+    double sclstartwire = sclstart[0];
+    double sclstarttime = sclstart[1];
+    double sclendwire   = sclend[0];
+    double sclendtime   = sclend[1];
+    
+    double cl2startwire = cl2start[0];
+    double cl2starttime = cl2start[1];
+    double cl2endwire   = cl2end[0];
+    double cl2endtime   = cl2end[1];
 
-	 //13.5 ticks/wire. need to make this detector agnostic--spitz
-     double distance=sqrt((pow(sclendwire-cl2startwire,2)*13.5)+pow(sclendtime-cl2starttime,2));
-     //not sure if this line is necessary--spitz
-     double distance2=sqrt((pow(sclstartwire-cl2endwire,2)*13.5)+pow(sclstarttime-cl2endtime,2));
+    // \todo 13.5 ticks/wire. need to make this detector agnostic--spitz
+    double distance = sqrt((pow(sclendwire-cl2startwire,2)*13.5) + pow(sclendtime-cl2starttime,2));
 
-    bool comp = (distance<fEndpointWindow||distance2<fEndpointWindow) ? true : false;
+    //not sure if this line is necessary--spitz
+    double distance2 = sqrt((pow(sclstartwire-cl2endwire,2)*13.5) + pow(sclstarttime-cl2endtime,2));
+    
+    bool comp = (distance  < fEndpointWindow ||
+		 distance2 < fEndpointWindow) ? true : false;
+
     return comp;
   }
 
-}//namespace cluster{
+}//namespace cluster
