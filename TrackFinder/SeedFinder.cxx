92dfb4cf37f18181713d75978475dbd33b1e4683
Benjamin Jones <bjpjones@fnal.gov>
2012-04-12 15:42:57 +0000

    SeedFinder service to module conversion
    Former-commit-id: 815a4518e0cc123ad0a906569dcdcb2fa6cd0b66

--- TrackFinder/SeedFinder.cxx
+++ TrackFinder/SeedFinder.cxx
@@ -9,7 +9,6 @@
 #include <vector>
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "TrackFinder/SeedFinder.h"
-#include "TrackFinder/SeedFinderService.h"
 #include "TrackFinder/SpacePointService.h"
 #include "Geometry/geo.h"
 #include "art/Framework/Principal/Event.h"
@@ -44,10 +43,16 @@ namespace trkf {
 
   void SeedFinder::reconfigure(fhicl::ParameterSet const& pset)
   {
-    fClusterModuleLabel = pset.get<std::string>("ClusterModuleLabel");
-    fFilter             = pset.get<bool>("Filter");
-    fMerge              = pset.get<bool>("Merge");
-    fSeedMode           = pset.get<int>("SeedMode");
+    fClusterModuleLabel    = pset.get<std::string>("ClusterModuleLabel");
+    fFilter                = pset.get<bool>("Filter");
+    fMerge                 = pset.get<bool>("Merge");
+    fSeedMode              = pset.get<int>("SeedMode");
+    fSeedLength            = pset.get<double>("SeedLength");
+    fMinPointsInCluster    = pset.get<unsigned int>("MinPointsInCluster");
+    fMinPointsInSeed       = pset.get<unsigned int>("MinPointsInSeed");
+    fAngularDev            = pset.get<double>("AngularDev");
+
+
   }
 
   void SeedFinder::beginJob()
@@ -55,17 +60,10 @@ namespace trkf {
 
   void SeedFinder::produce(art::Event& evt)
   {
-    art::ServiceHandle<trkf::SeedFinderService> sfsvc;  
- 
-    int EventNumber = evt.id().event();
-    sfsvc->SetEventID(EventNumber);
-
-    art::Handle< std::vector<recob::Cluster> > clusterh;
-    evt.getByLabel(fClusterModuleLabel, clusterh);
-
+    
     std::auto_ptr<std::vector<recob::Seed> > seeds(new std::vector<recob::Seed>);
 
-    std::vector<std::vector<recob::SpacePoint> > SpacePointVectors = GetSpacePoints(clusterh);
+    std::vector<std::vector<recob::SpacePoint> > SpacePointVectors = GetSpacePointsFromClusters(fClusterModuleLabel, evt);
     if(SpacePointVectors.size() > 0)
       {
 	for(std::vector<std::vector<recob::SpacePoint> >::const_iterator it=SpacePointVectors.begin(); 
@@ -74,9 +72,9 @@ namespace trkf {
 	  {
 	    std::vector<recob::Seed*> SeedFinderOutput;
 	    if(fSeedMode==0)
-	      SeedFinderOutput = sfsvc->FindSeedExhaustively(*it);
+	      SeedFinderOutput = FindSeedExhaustively(*it);
 	    else if(fSeedMode==1)
-	      SeedFinderOutput = sfsvc->FindAsManySeedsAsPossible(*it);
+	      SeedFinderOutput = FindAsManySeedsAsPossible(*it);
 	    else 
 	      throw cet::exception("SeedFinder") << 
 		"Unkown seed mode " << fSeedMode<<"\n";
@@ -84,8 +82,7 @@ namespace trkf {
 	    if(SeedFinderOutput.size()>0)
 	      for(unsigned int i=0; i!=SeedFinderOutput.size(); i++)
 		if(SeedFinderOutput.at(i)->IsValid())
-		  seeds->push_back(*(SeedFinderOutput.at(i)));
-	    
+		  seeds->push_back(*(SeedFinderOutput.at(i)));	    
 	  }			  
       }
     else
@@ -95,12 +92,20 @@ namespace trkf {
     
   }
 
-  std::vector<std::vector<recob::SpacePoint> > SeedFinder::GetSpacePoints(art::Handle<std::vector<recob::Cluster> > clusterh)
+
+
+  // Given a cluster label, produce spacepoints from the event
+  //  very possible this spacepoint supply will be swapped out later.
+
+  std::vector<std::vector<recob::SpacePoint> > SeedFinder::GetSpacePointsFromClusters(std::string ClusterModuleLabel, art::Event& evt)
   {
     // Get Services.
 
     art::ServiceHandle<trkf::SpacePointService> sptsvc;
     art::ServiceHandle<geo::Geometry> geom;
+
+    art::Handle< std::vector<recob::Cluster> > clusterh;
+    evt.getByLabel(ClusterModuleLabel, clusterh);
 
     // Get clusters.
 
@@ -211,9 +216,219 @@ namespace trkf {
 
     }
     return SpacePointVectors;
-
-
   }
+
+
+
+
+
+  //
+  // Take a collection of spacepoints and return a
+  // vector of as many straight line seeds as possible
+  //
+  std::vector<recob::Seed *> SeedFinder::FindAsManySeedsAsPossible(std::vector<recob::SpacePoint> Points)
+  {
+    std::vector<recob::Seed*> ReturnVector;
+
+    recob::Seed* TrackSeedThisCombo;
+    bool KeepChopping=true;
+    if(Points.size()<fMinPointsInCluster) KeepChopping=false;
+    while(KeepChopping)
+      {
+	TrackSeedThisCombo = FindSeedAtEnd(Points);
+	if(TrackSeedThisCombo->IsValid())
+	  {
+	    ReturnVector.push_back(TrackSeedThisCombo);
+	  }
+	// if enough left, chop off some points and try again
+	if(Points.size() > fMinPointsInSeed)
+	  Points.resize(Points.size()-fMinPointsInSeed);
+	else  // or if there is nothing left to chop, leave the loop
+	  KeepChopping=false;
+
+      }
+
+    return ReturnVector;
+  }
+
+
+
+
+
+
+  //
+  // Take a collection of spacepoints sorted in Z and find
+  // exactly one straight seed, as high in Z as possible.
+  //
+  std::vector<recob::Seed*> SeedFinder::FindSeedExhaustively(std::vector<recob::SpacePoint> Points)
+  {
+    recob::Seed* TrackSeedThisCombo;
+    bool KeepChopping=true;
+    bool FoundValidSeed=false;
+    if(Points.size()<=fMinPointsInCluster) KeepChopping=false;
+    while(KeepChopping)
+      {
+	TrackSeedThisCombo = FindSeedAtEnd(Points);
+	if(TrackSeedThisCombo->IsValid())
+	  {
+	    KeepChopping=false;
+	    FoundValidSeed=true;
+	  }
+	else
+	  {
+	    // if we found an invalid or weak seed, chop off some points and try again
+  if(Points.size() > fMinPointsInSeed)
+    Points.resize(Points.size()-fMinPointsInSeed);
+  else  // or if there is nothing left to chop, leave the loop
+    KeepChopping=false;
+	  }
+      }
+    if(!FoundValidSeed)
+      {
+	TrackSeedThisCombo = new recob::Seed();
+      }
+    else
+      {
+	double SeedPoint[3], SeedErr[3];
+	TrackSeedThisCombo->GetPoint(SeedPoint,SeedErr);
+	std::cout<<"FindSeedEx : " << SeedPoint[0]<< " " <<
+	  SeedPoint[1]<< " " <<
+	  SeedPoint[2]<< " " << std::endl;
+      }
+    std::vector<recob::Seed*> ReturnVector;
+    ReturnVector.push_back(TrackSeedThisCombo);
+    return ReturnVector;
+  }
+
+
+  //
+  // Start with a vector of hits ordered in Z.
+  // Put together a seed using the last N hits, find its
+  // centre, direction and strength. Return this seed for
+  // further scrutiny.
+  //
+  recob::Seed * SeedFinder::FindSeedAtEnd(std::vector<recob::SpacePoint> Points)
+  {
+
+    recob::Seed * ReturnSeed;
+
+    std::vector<int> PointsInRange;
+    PointsInRange.clear();
+
+    TVector3 HighestZPoint( Points.at(Points.size()-1).XYZ()[0],
+			    Points.at(Points.size()-1).XYZ()[1],
+			    Points.at(Points.size()-1).XYZ()[2] );
+
+    TVector3 CentreOfPoints(0.,0.,0.);
+
+    for(int index=Points.size()-1; index!=-1; --index)
+      TVector3 CentreOfPoints(0.,0.,0.);
+
+    for(int index=Points.size()-1; index!=-1; --index)
+      {
+	// first check z, then check total distance
+	//  (much faster, since most will be out of range in z anyway)
+
+
+	if( ( HighestZPoint[2] - Points.at(index).XYZ()[2] ) < fSeedLength)
+	  {
+	    if( ( ( pow(HighestZPoint[0]-Points.at(index).XYZ()[0],2) +
+		    pow(HighestZPoint[1]-Points.at(index).XYZ()[1],2) +
+		    pow(HighestZPoint[2]-Points.at(index).XYZ()[2],2) ) < pow(fSeedLength,2)))
+	      {
+		PointsInRange.push_back(index);
+		CentreOfPoints[0]+=Points.at(index).XYZ()[0];
+		CentreOfPoints[1]+=Points.at(index).XYZ()[1];
+		CentreOfPoints[2]+=Points.at(index).XYZ()[2];
+	      }
+	  }
+      }
+
+    unsigned int NPoints = PointsInRange.size();
+    if(NPoints==0) return new recob::Seed();
+    CentreOfPoints = (1./float(NPoints)) * CentreOfPoints;
+
+    if(NPoints>fMinPointsInSeed)
+      {
+	float costheta=0, phi=0, costheta2=0, phi2=0;
+
+	for(unsigned int i=0; i!=PointsInRange.size(); i++)
+	  {
+	    TVector3 ThisPoint(Points.at(PointsInRange.at(i)).XYZ()[0],
+			       Points.at(PointsInRange.at(i)).XYZ()[1],
+			       Points.at(PointsInRange.at(i)).XYZ()[2]);
+	    TVector3 Step = CentreOfPoints-ThisPoint;
+
+	    if(Step.Z()<0) Step=-Step;
+
+
+	    phi       += Step.Phi();
+	    phi2      += pow(Step.Phi(),2);
+
+	    // Need to check range of costheta to avoid getting a nan.
+	    //  (if CosTheta=1, floating point errors mess us up)
+	    if(Step.CosTheta()<0.9999)
+	      {
+		costheta     += Step.CosTheta();
+		costheta2    += pow(Step.CosTheta(),2);
+	      }
+	    else
+	      {
+		costheta+=1;
+		costheta2+=1;
+	      }
+	    //      std::cout<<phi<< " " <<phi2<< " " <<costheta << " " <<costheta2<\
+	      <std::endl;
+	  }
+
+
+	float sigtheta    = pow((costheta2 / NPoints - pow( costheta/NPoints, 2 )), 0.5);
+	float sigphi      = pow((phi2      / NPoints - pow( phi/NPoints,   2 )),    0.5);
+	float meantheta   = acos(costheta      / NPoints);
+	float meanphi     = phi                / NPoints;
+
+	// Total angular deviation (spherical polars)
+	float AngularDev =  pow(pow(sigtheta,2) + pow(sin(sigtheta) * sigphi, 2), 0.5);
+
+	std::cout<<"SeedFinder debug: " << AngularDev<<std::endl;
+
+	if(AngularDev<fAngularDev)
+	  {
+
+	    double PtArray[3], DirArray[3];
+
+	    PtArray[0]=CentreOfPoints.X();
+	    PtArray[1]=CentreOfPoints.Y();
+	    PtArray[2]=CentreOfPoints.Z();
+
+	    // calculate direction from average over hits
+
+	    TVector3 SeedDirection;
+
+	    SeedDirection.SetMagThetaPhi(fSeedLength, meantheta, meanphi);
+
+	    DirArray[0]=SeedDirection.X();
+	    DirArray[1]=SeedDirection.Y();
+	    DirArray[2]=SeedDirection.Z();
+
+	    ReturnSeed = new recob::Seed(PtArray,DirArray);
+	  }
+        else ReturnSeed = new recob::Seed();
+      }
+    else ReturnSeed = new recob::Seed();
+
+    return ReturnSeed;
+  }
+
+
+
+
+
+
+
+
+
+
 
   void SeedFinder::endJob()
   {
--- TrackFinder/SeedFinder.h
+++ TrackFinder/SeedFinder.h
@@ -5,15 +5,32 @@
 // Name: SeedFinder.h
 //
 // Purpose: Header file for module SeedFinder.  This modules makes
-//          seeds using the SeedFinderService
+//           seeds from clusters, via spacepoints.
+//          Seeds must pass a point multiplicity requirement,
+//           and a directionality cut.
+//          There are 2 modes: 
+//           1: Find one seed (if possible) in each cluster combo 
+//           2: Find as many seeds as possible
 //
 // Configuration parameters.
 //
 // ClusterModuleLabel;  // Cluster module label (e.g. "dbcluster").
 // Filter;              // Filter space points?
 // Merge;               // Merge space points?
+// fSeedMode;           // 1: Find 1 seed per track at high Z
+//                      // 2: Find as many seeds as possible
+// fSeedLength;         // Distance from highest Z point to search
+//                      //  for hits
+// fMinPointsInCluster; // How many SP's required in a cluster before
+//                      //  trying to seed?
+// fMinPointsInSeed;    // How many nearby points required to define
+//                      //  a seed?
+// fAngularDev;         // Angular standard deviation in radians
+//                      //  for seed to meet directionality requirement
+
 //
-// Ben Jones, MIT
+// Ben Jones, MIT, April 2012
+//   bjpjones@mit.edu
 //
 
 #include "art/Framework/Core/EDProducer.h"
@@ -22,6 +39,7 @@ namespace recob
 {
   class SpacePoint;
   class Cluster;
+  class Seed;
 }
 
 namespace trkf {
@@ -41,19 +59,55 @@ namespace trkf {
     void beginJob();
     void produce(art::Event& evt);
     void endJob();
+  
     
-    std::vector<std::vector<recob::SpacePoint> > GetSpacePoints(art::Handle<std::vector<recob::Cluster> >);
-    
+
 
   private:
+    
+                   
+    std::vector<std::vector<recob::SpacePoint> > GetSpacePointsFromClusters(std::string ClusterModuleLabel, art::Event & evt);
+              // Get space points from clusters
+              //    (I expect eventually we will get from event, which
+              //     will be a separate method here) 
 
+
+    recob::Seed * FindSeedAtEnd(std::vector<recob::SpacePoint>);
+              // Find one seed at a high Z end of a collection of spacepoints
+              //    with no quality check
+
+
+    
+    std::vector<recob::Seed *> FindSeedExhaustively(std::vector<recob::SpacePoint>);
+              // Generate seed at high Z end, check quality, discard if not adequate
+              //    repeat until a strong seed found
+
+
+ 
+    std::vector<recob::Seed *> FindAsManySeedsAsPossible(std::vector<recob::SpacePoint>);
+              // Starting at high Z, find as many adequate seeds in the collection
+              //     as possible
+
+    
     // Fcl Attributes.
 
-    std::string fClusterModuleLabel;
-    bool fFilter, fMerge, fMakeEvdTracks;
-    int fSeedMode;
-    
+    std::string fClusterModuleLabel;           // Where to find clusters for this algorithm
+ 
+    bool            fFilter, fMerge;           // Space point service settings
+                                               //  See that service for info.
+    int             fSeedMode;                 // 1: Find 1 seed per track at high Z
+                                               // 2: Find as many seeds as possible 
+    double          fSeedLength;               // Distance from highest Z point to search
+                                               //  for hits
+    unsigned int    fMinPointsInCluster;       // How many SP's required in a cluster before
+                                               //  trying to seed?
+    unsigned int    fMinPointsInSeed;          // How many nearby points required to define
+                                               //  a seed?
+    float           fAngularDev;               // Angular standard deviation in radians
+                                               //  for seed to meet directionality requirement
+
+
   };
 }
 
-#endif // SPACEPOINTFINDER_H
+#endif // SEEDFINDER_H
