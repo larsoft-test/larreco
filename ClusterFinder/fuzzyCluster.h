284f1e6c5250378cbe0f11ebac71658704de8a86
Benjamin Carls <bcarls@fnal.gov>
2012-08-07 19:24:41 +0000

    Adding in fuzzy clustering algorithm
    Former-commit-id: 4ec18bc2fc36ea9d4c2aec656dc4c1b11ecb9956

---
+++ ClusterFinder/fuzzyCluster.h
@@ -0,0 +1,38 @@
+/////////////////////////////////////////////////////////////////
+//  \file fuzzyCluster.h
+//  kinga.partyka@yale.edu
+////////////////////////////////////////////////////////////////////
+
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include "art/Framework/Core/EDProducer.h"
+#include "ClusterFinder/fuzzyClusterAlg.h"
+//#include "ClusterFinder/HoughClusAlg.h"
+
+class TH1F;
+
+namespace cluster{
+   
+  //--------------------------------------------------------------- 
+  class fuzzyCluster : public art::EDProducer
+  {
+  public:
+    explicit fuzzyCluster(fhicl::ParameterSet const& pset); 
+    ~fuzzyCluster();
+    void produce(art::Event& evt);
+    void beginJob();
+    void reconfigure(fhicl::ParameterSet const& p);
+    
+  private:
+       
+    TH1F *fhitwidth;
+    TH1F *fhitwidth_ind_test;  
+    TH1F *fhitwidth_coll_test;  
+   
+    std::string fhitsModuleLabel;
+   
+    fuzzyClusterAlg ffuzzyCluster; ///< object that implements the fuzzy cluster algorithm
+  };
+
+}
