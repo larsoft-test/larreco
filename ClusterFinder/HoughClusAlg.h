eb03113ce1d543529c492ffb5f0458552a4f0d9e
Benjamin Carls <bcarls@fnal.gov>
2012-08-28 01:10:20 +0000

    Changing art::PtrVector to std::vector<art::Ptr<>>
    Former-commit-id: 2e7e8026b816a1044492aa184c23fffae2586aca

--- ClusterFinder/HoughClusAlg.h
+++ ClusterFinder/HoughClusAlg.h
@@ -32,7 +32,13 @@ namespace recob {
       double pMax0;
       double pMax1;
       bool merged;
-      lineSlope(unsigned int num, double slope, double intercept, double Min0, double Min1, double Max0, double Max1)
+      lineSlope(unsigned int num, 
+          double slope, 
+          double intercept, 
+          double Min0, 
+          double Min1, 
+          double Max0, 
+          double Max1)
       {
         clusterNumber = num;
         oldClusterNumber = num;
@@ -109,21 +115,20 @@ namespace cluster {
     HoughClusAlg(fhicl::ParameterSet const& pset); 
     virtual ~HoughClusAlg();
          
-    size_t Transform(art::PtrVector<recob::Cluster>                 & clusIn,
-     	             std::vector<recob::Cluster>               	    & ccol,  
-		     std::vector< art::PtrVector<recob::Hit> >      & clusHitsOut,
+    size_t Transform(std::vector<art::Ptr<recob::Cluster> >           & clusIn,
+     	             std::vector<recob::Cluster>                      & ccol,  
+		     std::vector<std::vector<art::Ptr<recob::Hit> > > & clusHitsOut,
 		     art::Event                                const& evt,
 		     std::string                               const& label);
 
-    size_t Transform(art::PtrVector<recob::Hit>& hits,
+    size_t Transform(std::vector<art::Ptr<recob::Hit> >& hits,
                      std::vector<unsigned int>     *fpointId_to_clusterId,
                      unsigned int clusterId, // The id of the cluster we are examining
                      int *nClusters,
                      std::vector<unsigned int> corners);
 
 
-    //size_t Transform(std::vector< art::Ptr<recob::Hit> >& hits);
-    size_t Transform(art::PtrVector<recob::Hit>& hits);
+    size_t Transform(std::vector<art::Ptr<recob::Hit> >& hits);
 
     void reconfigure(fhicl::ParameterSet const& pset);
           
@@ -152,16 +157,51 @@ namespace cluster {
     double fLineIsolationCut;
     double fHoughLineMergeCutoff;
     double fParaHoughLineMergeCutoff;
-    void mergeHoughLines(unsigned int k,std::vector<lineSlope> *linesFound, std::vector<int> *newClusNum, std::vector<double> *newClusDist, double xyScale);
-    void mergeHoughLinesBySegment(unsigned int k,std::vector<lineSlope> *linesFound, std::vector<int> *newClusNum, std::vector<double> *newClusDist, double xyScale);
-    int mergeParaHoughLines(unsigned int k,std::vector<lineSlope> *linesFound, std::vector<int> *newClusNum, std::vector<double> *newClusDist, double xyScale);
-    void mergeParaHoughLinesBySegment(unsigned int k,std::vector<lineSlope> *linesFound, std::vector<int> *newClusNum, std::vector<double> *newClusDist, double xyScale);
+    void mergeHoughLines(unsigned int k,
+        std::vector<lineSlope> *linesFound, 
+        std::vector<int> *newClusNum, 
+        std::vector<double> *newClusDist, 
+        double xyScale);
+    void mergeHoughLinesBySegment(unsigned int k,
+        std::vector<lineSlope> *linesFound, 
+        std::vector<int> *newClusNum, 
+        std::vector<double> *newClusDist, 
+        double xyScale);
+    int mergeParaHoughLines(unsigned int k,
+        std::vector<lineSlope> *linesFound, 
+        std::vector<int> *newClusNum, 
+        std::vector<double> *newClusDist, 
+        double xyScale);
+    void mergeParaHoughLinesBySegment(unsigned int k,
+        std::vector<lineSlope> *linesFound, 
+        std::vector<int> *newClusNum, 
+        std::vector<double> *newClusDist, 
+        double xyScale);
      
 
     //std::vector<lineSlope> linesFound;
-    double HoughLineDistance(double p0MinLine1, double p1MinLine1, double p0MaxLine1, double p1MaxLine1, double p0MinLine2, double p1MinLine2, double p0MaxLine2, double p1MaxLine2);
-    bool   HoughLineIntersect(double x11,double  y11,double  x12,double  y12,double  x21,double  y21,double  x22,double  y22);
-    double PointSegmentDistance(double px,double  py,double  x1,double  y1,double  x2,double  y2);
+    double HoughLineDistance(double p0MinLine1, 
+        double p1MinLine1, 
+        double p0MaxLine1, 
+        double p1MaxLine1, 
+        double p0MinLine2, 
+        double p1MinLine2, 
+        double p0MaxLine2, 
+        double p1MaxLine2);
+    bool   HoughLineIntersect(double x11,
+        double  y11,
+        double  x12,
+        double  y12,
+        double  x21,
+        double  y21,
+        double  x22,
+        double  y22);
+    double PointSegmentDistance(double px,
+        double  py,
+        double  x1,
+        double  y1,
+        double  x2,
+        double  y2);
 
 
     
