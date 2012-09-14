74d127f974a39ab766f8eb4bd73320b3328f5cd2
Benjamin Carls <bcarls@fnal.gov>
2012-09-14 18:09:01 +0000

    Setting random number seed needed for PPHT to event number
    Former-commit-id: 46efe4fb5fc66ca141a3d6d35c84b8a94e004409

--- ClusterFinder/HoughLineAlg.h
+++ ClusterFinder/HoughLineAlg.h
@@ -16,6 +16,8 @@
 #include "art/Persistency/Common/Ptr.h" 
 #include "art/Persistency/Common/PtrVector.h" 
 
+#include "ClusterFinder/HoughBaseAlg.h"
+
 namespace recob { 
   class Hit;
   class Cluster; 
@@ -23,50 +25,7 @@ namespace recob {
 
 namespace cluster {
    
-  class HoughTransform {
-  public:
-    
-    HoughTransform();
-    ~HoughTransform();
-     
-    void Init(int dx, int dy, int rhoresfact, int numACells);
-    bool AddPoint(int x, int y);
-    int  GetCell(int row, int col)            { return m_accum[row][col]; }
-    void SetCell(int row, int col, int value) { m_accum[row][col] = value; }
-    void IncrementCell(int row, int col)      { m_accum[row][col]++;}
-    void GetAccumSize(int &numRows, int &numCols) 
-    { 
-      numRows = m_accum.size();
-      numCols  = m_rowLength;
-    }
-    int NumAccumulated()                      { return m_numAccumulated; }
-    void GetEquation(double row, double col, double &rho, double &theta)
-    {
-      theta = (TMath::Pi()*row)/m_numAngleCells;
-      rho   = (col - (m_rowLength/2.))/m_rhoResolutionFactor;
-    }
-    int GetMax(int & xmax, int & ymax);
-      
-    private:
-         
-    int m_dx;
-    int m_dy;
-    std::vector<std::map<int,int> > m_accum;  // column=rho, row=theta
-    //int distCenter;// \todo Why is this here? Only used locally by DoAddPoint
-    //int lastDist;
-    //int dist;
-    //int stepDir; 
-    //int cell;
-    int m_rowLength;
-    int m_numAccumulated;
-    int m_rhoResolutionFactor;
-    int m_numAngleCells;
-    std::vector<double> m_cosTable;
-    std::vector<double> m_sinTable;
-    bool DoAddPoint(int x, int y);
-  }; // class HoughTransform  
-
-  class HoughLineAlg {
+  class HoughLineAlg:public HoughBaseAlg {
     
   public:
     
@@ -86,9 +45,6 @@ namespace cluster {
     void reconfigure(fhicl::ParameterSet const& pset);
           
   private:
-  
-    void HLSSaveBMPFile(char const*, unsigned char*, int, int);
-
   
     int    fMaxLines;         ///< Max number of lines that can be found 
     int    fMinHits;          ///< Min number of hits in the accumulator to consider 
