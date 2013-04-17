285a81462a13d897f06e005ae73652018e022f8e
Jonathan Asaadi <jasaadi@fnal.gov>
2013-04-17 00:38:22 +0000

    First commit of CornerFinderAlg. This compiles but has not been test driven...don't use yet...work done by Wes, Ben, Jonathan...mostly Wes
    Former-commit-id: d25e36a7fcb67c965154efd5092804abab314e23

---
+++ ClusterFinder/CornerFinderAlg.cxx
@@ -0,0 +1,417 @@
+////////////////////////////////////////////////////////////////////////
+//
+// CornerFinderAlg class
+//
+// yo-mama@state.edu
+//
+//  blah blah blah...put description here 
+//  
+//  
+//  
+////////////////////////////////////////////////////////////////////////
+
+
+// ### Lots of includes...not sure if we need them all...just copy and pasting
+#include <iostream>
+#include <vector>
+extern "C" {
+#include <sys/types.h>
+#include <sys/stat.h>
+}
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <algorithm>
+
+#include "TMath.h"
+
+// Framework includes
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Framework/Services/Optional/TFileDirectory.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "RawData/raw.h"
+#include "RawData/RawDigit.h"
+#include "Filters/ChannelFilter.h"
+#include "Geometry/CryostatGeo.h"
+#include "Geometry/TPCGeo.h"
+#include "Geometry/PlaneGeo.h"
+#include "Geometry/Geometry.h"
+#include "RecoBase/Cluster.h"
+#include "ClusterFinder/CornerFinderAlg.h"
+
+
+
+#include "art/Framework/Core/ModuleMacros.h" 
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Principal/Event.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Persistency/Common/Ptr.h"
+#include "art/Persistency/Common/PtrVector.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Framework/Services/Optional/TFileDirectory.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+
+// NOTE: In the .h file I assumed this would belong in the cluster class....if 
+// we decide otherwise we will need to search and replace for this
+
+
+//-----------------------------------------------------------------------------
+cluster::CornerFinderAlg::CornerFinderAlg(fhicl::ParameterSet const& pset) 
+// ### This is where we take in the RawData from the .fcl file
+  :fRawDataLabel (pset.get<std::string>("RawDataLabel"))
+{
+  this->reconfigure(pset);
+}
+
+//-----------------------------------------------------------------------------
+cluster::CornerFinderAlg::~CornerFinderAlg()
+{
+}
+
+//-----------------------------------------------------------------------------
+void cluster::CornerFinderAlg::reconfigure(fhicl::ParameterSet const& p)
+{
+  // ### These are all the tuneable .fcl file parameters from the event ###
+  fConversion_threshold     		 = p.get< float    	 >("Conversion_threshold");
+  fConversion_bins_per_input_x  	 = p.get< int      	 >("Conversion_bins_per_input_x");
+  fConversion_bins_per_input_y       	 = p.get< int      	 >("Conversion_bins_per_input_y");
+  fDerivative_method        		 = p.get< std::string    >("Derivative_method");
+  fCornerScore_neighborhood     	 = p.get< int		 >("CornerScore_neighborhood");
+  fCornerScore_algorithm		 = p.get< std::string    >("CornerScore_algorithm");
+  fMaxSuppress_neighborhood		 = p.get< int		 >("MaxSupress_neighborhood");
+  fMaxSuppress_threshold			 = p.get< int		 >("MaxSupress_threshold");
+}
+
+
+//-----------------------------------------------------------------------------
+TH2F* cluster::CornerFinderAlg::TakeInRaw(art::PtrVector<raw::RawDigit>	& rawhits,
+					   art::Event				const&evt)
+
+{
+  // Use the TFile service in art
+  art::ServiceHandle<art::TFileService> tfs;
+  
+  // Make a vector for the raw hits
+  art::PtrVector<raw::RawDigit> RawHits;
+  RawHits.clear();
+  
+  
+  // Push the raw hits into the vector
+  art::Handle< std::vector<raw::RawDigit> >  rawdigitcol;
+  try{
+    evt.getByLabel(fRawDataLabel, rawdigitcol);
+    for(unsigned int i = 0; i < rawdigitcol->size(); ++i){
+      art::Ptr<raw::RawDigit> rawdigit(rawdigitcol, i);
+      RawHits.push_back(rawdigit);
+    }
+  }
+  // Catch if things go badly
+  catch(cet::exception& excep){
+   std::cout<<"Bail out!"<<std::endl;;
+  }
+   
+  // Getting the bins for the histograms in terms of number of wires and number of time ticks
+  art::ServiceHandle<geo::Geometry> geom;
+  const unsigned int nPlanes = geom->Nplanes();
+  const unsigned int nWires = geom->Nwires(0);
+  const unsigned int nTimeTicks = (RawHits.at(0))->Samples();
+  
+  
+  // Initializing the histograms
+  
+  /// \ todo: Add in a loop over planes to fill this correctly
+  RawData_histos[0] = tfs->make<TH2F>("RawData_Plane_0","Raw Data for plane 0",nWires,0,nWires,nTimeTicks,0,nTimeTicks);
+  RawData_histos[1] = tfs->make<TH2F>("RawData_Plane_0","Raw Data for plane 0",nWires,0,nWires,nTimeTicks,0,nTimeTicks);
+  RawData_histos[2] = tfs->make<TH2F>("RawData_Plane_0","Raw Data for plane 0",nWires,0,nWires,nTimeTicks,0,nTimeTicks);
+  
+  // Loop over the RawHits
+  for (auto const& hit : RawHits) {
+    // Adding a protection in case we don't have enough RawHits
+    if(RawHits.size()<1) continue;
+    
+    
+    geo::SigType_t SignalType = geom->SignalType(hit->Channel());
+    
+    std::vector<short> uncompressed(hit->Samples());
+    raw::Uncompress(hit->fADC, uncompressed, hit->Compression());
+    
+    std::vector<geo::WireID> WireId = geom->ChannelToWire(hit->Channel());
+    for(auto const& wid : WireId){
+      
+      unsigned int plane = wid.Plane;
+      unsigned int wire = wid.Wire;
+      for(int time = 0; time < nTimeTicks; time++)
+	RawData_histos[plane]->SetBinContent(wire,time,(uncompressed.at(time)) - hit->GetPedestal());
+
+    } // end loop over wires
+  } // end loop over rawhits
+   
+ 
+ // Give me pointer to get out the TH2D here!
+ 
+ // Wacky hard coded things....fix me
+ 
+ 
+ run(RawData_histos[0], geom->View(0) );
+
+}//<---End TakeInRaw
+
+
+
+/// All other methods go below here.....................
+
+//-----------------------------------------------------------------------------
+// Convert to pixel
+void cluster::CornerFinderAlg::create_image_histo(TH2F *h_wire_data, TH2F *h_conversion) {
+  
+  const int x_bins = h_wire_data->GetNbinsX();
+  const float x_min = h_wire_data->GetXaxis()->GetBinLowEdge(1);
+  const float x_max = h_wire_data->GetXaxis()->GetBinUpEdge(x_bins);
+
+  const int y_bins = h_wire_data->GetNbinsY();
+  const float y_min = h_wire_data->GetYaxis()->GetBinLowEdge(1);
+  const float y_max = h_wire_data->GetYaxis()->GetBinUpEdge(y_bins);
+
+  const int converted_y_bins = y_bins/fConversion_bins_per_input_y;
+  const int converted_x_bins = x_bins/fConversion_bins_per_input_x;
+
+  h_conversion = new TH2F("h_conversion","Image Conversion Histogram",
+			  converted_x_bins,x_min,x_max,
+			  converted_y_bins,y_min,y_max);
+
+  int counter_ybin=1;
+  int counter_xbin=1;
+  float temp_sum=0;
+
+  for(int iy=1; iy<=converted_y_bins; iy++){
+    for(int ix=1; ix<=converted_x_bins; ix++){
+
+      temp_sum=0;
+      for(int jy=counter_ybin; jy<counter_ybin+fConversion_bins_per_input_y; jy++){
+	for(int jx=counter_xbin; jx<counter_xbin+fConversion_bins_per_input_x; jx++){
+	  temp_sum += h_wire_data->GetBinContent(jx,jy);
+	}
+      }
+      
+      counter_ybin += fConversion_bins_per_input_y;
+      counter_xbin += fConversion_bins_per_input_x;
+
+
+      if( temp_sum > fConversion_threshold)
+	h_conversion->SetBinContent(ix,iy,temp_sum);
+    }
+  }
+
+
+  return;
+}
+
+//-----------------------------------------------------------------------------
+// Derivative
+
+void cluster::CornerFinderAlg::create_derivative_histograms(TH2F *h_conversion, TH2F *h_derivative_x, TH2F *h_derivative_y){
+
+  const int x_bins = h_conversion->GetNbinsX();
+  const float x_min = h_conversion->GetXaxis()->GetBinLowEdge(1);
+  const float x_max = h_conversion->GetXaxis()->GetBinUpEdge(x_bins);
+
+  const int y_bins = h_conversion->GetNbinsY();
+  const float y_min = h_conversion->GetYaxis()->GetBinLowEdge(1);
+  const float y_max = h_conversion->GetYaxis()->GetBinUpEdge(y_bins);
+
+  h_derivative_x = new TH2F("h_derivative_x","Partial Derivatives (x)",
+			    x_bins,x_min,x_max,
+			    y_bins,y_min,y_max);
+  h_derivative_y = new TH2F("h_derivative_y","Partial Derivatives (y)",
+			    x_bins,x_min,x_max,
+			    y_bins,y_min,y_max);
+
+  for(int iy=1; iy<=y_bins; iy++){
+    for(int ix=1; ix<=x_bins; ix++){
+      
+      //set to zero the derivatives near the corners...
+      if ( iy <= fDerivative_neighborhood 
+	   || ix <= fDerivative_neighborhood 
+	   || iy >= (y_bins-fDerivative_neighborhood) 
+	   || ix == (x_bins-fDerivative_neighborhood) )
+	{
+	  h_derivative_x->SetBinContent(ix,iy,0);
+	  h_derivative_y->SetBinContent(ix,iy,0);
+	}
+      
+      else
+	{
+	  h_derivative_x->SetBinContent(ix,iy,
+					2*(h_conversion->GetBinContent(ix+1,iy)-h_conversion->GetBinContent(ix-1,iy))
+					+ 1*(h_conversion->GetBinContent(ix+1,iy+1)-h_conversion->GetBinContent(ix-1,iy+1))
+					+ 1*(h_conversion->GetBinContent(ix+1,iy-1)-h_conversion->GetBinContent(ix-1,iy-1)));
+	  h_derivative_y->SetBinContent(ix,iy,
+					2*(h_conversion->GetBinContent(ix,iy+1)-h_conversion->GetBinContent(ix,iy-1))
+					+ 1*(h_conversion->GetBinContent(ix-1,iy+1)-h_conversion->GetBinContent(ix-1,iy-1))
+					+ 1*(h_conversion->GetBinContent(ix+1,iy+1)-h_conversion->GetBinContent(ix+1,iy-1)));
+	}
+    }
+  }
+
+  return;
+
+}
+
+
+//-----------------------------------------------------------------------------
+// Corner Score
+
+void cluster::CornerFinderAlg::create_cornerScore_histogram(TH2F *h_derivative_x, TH2F *h_derivative_y, TH2D *h_cornerScore){
+
+  const int x_bins = h_derivative_x->GetNbinsX();
+  const float x_min = h_derivative_x->GetXaxis()->GetBinLowEdge(1);
+  const float x_max = h_derivative_x->GetXaxis()->GetBinUpEdge(x_bins);
+
+  const int y_bins = h_derivative_y->GetNbinsY();
+  const float y_min = h_derivative_y->GetYaxis()->GetBinLowEdge(1);
+  const float y_max = h_derivative_y->GetYaxis()->GetBinUpEdge(y_bins);
+
+  const double noble_epsilon = 1e-5;
+  const double harris_kappa = 0.05;
+
+  h_cornerScore = new TH2D("h_cornerScore","Feature Point Corner Score",
+			   x_bins,x_min,x_max,
+			   y_bins,y_min,y_max);
+  
+  //the structure tensor elements
+  float st_xx, st_xy, st_yy;
+
+  for(int iy=1; iy<=y_bins; iy++){
+    for(int ix=1; ix<=x_bins; ix++){
+      
+      //ignore things that will push us over the histogram edge
+      if( ix-fCornerScore_neighborhood <= 0 
+	  || ix+fCornerScore_neighborhood >= x_bins
+	  || iy-fCornerScore_neighborhood <=0
+	  || iy+fCornerScore_neighborhood>=y_bins)
+	{
+	  continue;
+	}
+      
+
+      else{
+
+	st_xx=0.; st_xy=0.; st_yy=0.;
+
+	for(int jx=ix-fCornerScore_neighborhood; jx<=ix+fCornerScore_neighborhood; jx++){
+	  for(int jy=iy-fCornerScore_neighborhood; jy<=iy+fCornerScore_neighborhood; jy++){
+
+	    st_xx += h_derivative_x->GetBinContent(jx,jy)*h_derivative_x->GetBinContent(jx,jy);
+	    st_yy += h_derivative_y->GetBinContent(jx,jy)*h_derivative_y->GetBinContent(jx,jy);
+	    st_xy += h_derivative_x->GetBinContent(jx,jy)*h_derivative_y->GetBinContent(jx,jy);
+
+	  }
+	}
+
+	
+	h_cornerScore->SetBinContent(ix,iy,
+				     (st_xx*st_yy-st_xy*st_xy) / (st_xx+st_yy + noble_epsilon));
+      }
+
+    }
+  }
+
+  return;
+}
+
+
+//-----------------------------------------------------------------------------
+// Max Supress
+size_t cluster::CornerFinderAlg::perform_maximum_suppression(TH2D *h_cornerScore, 
+				   std::vector<recob::Corner> & corner_vector,
+				   geo::View_t view, 
+				   TH2D *h_maxSuppress=NULL){
+
+  const int x_bins = h_cornerScore->GetNbinsX();
+  const float x_min = h_cornerScore->GetXaxis()->GetBinLowEdge(1);
+  const float x_max = h_cornerScore->GetXaxis()->GetBinUpEdge(x_bins);
+
+  const int y_bins = h_cornerScore->GetNbinsY();
+  const float y_min = h_cornerScore->GetYaxis()->GetBinLowEdge(1);
+  const float y_max = h_cornerScore->GetYaxis()->GetBinUpEdge(y_bins);
+
+  if(h_maxSuppress) {
+    h_maxSuppress = new TH2D("h_maxSuppress","Corner Points (Maximum Suppressed)",
+			     x_bins,x_min,x_max,
+			     y_bins,y_min,y_max);
+  }
+
+  double temp_max;
+  bool temp_center_bin;
+
+  for(int iy=1; iy<=y_bins; iy++){
+    for(int ix=1; ix<=x_bins; ix++){
+
+      if(h_cornerScore->GetBinContent(ix,iy) < fMaxSuppress_threshold)
+	continue;
+
+      temp_max = -1000;
+      temp_center_bin = false;
+
+      for(int jx=ix-fMaxSuppress_neighborhood; jx<=ix+fMaxSuppress_neighborhood; jx++){
+	for(int jy=iy-fMaxSuppress_neighborhood; jy<=iy+fMaxSuppress_neighborhood; jy++){
+
+	  if(h_cornerScore->GetBinContent(jx,jy) > temp_max){
+	    temp_max = h_cornerScore->GetBinContent(jx,jy);
+	    if(jx==ix && jy==iy) temp_center_bin=true;
+	    else{ temp_center_bin=false; }
+	  }
+
+	}
+      }
+
+      if(temp_center_bin){
+	
+	float time_tick = fConversion_bins_per_input_y*(0.5 * (float)iy);
+	int wire_number = fConversion_bins_per_input_x*(0.5 * ix);
+	recob::Corner corner(time_tick,
+			     wire_number,
+			     h_cornerScore->GetBinContent(ix,iy),
+			     view);
+	corner_vector.push_back(corner);
+
+	if(h_maxSuppress)
+	  h_maxSuppress->SetBinContent(ix,iy,h_cornerScore->GetBinContent(ix,iy));	
+      }
+      
+    }
+  }
+  
+  return corner_vector.size();
+
+}
+
+
+
+void cluster::CornerFinderAlg::run(TH2F *h_wire_data, geo::View_t view){
+
+
+  TH2F *h_conversion;
+  create_image_histo(h_wire_data,h_conversion);
+  
+  TH2F *h_derivative_x, *h_derivative_y;
+  create_derivative_histograms(h_conversion,h_derivative_x,h_derivative_y);
+  
+  TH2D *h_cornerScore;
+  create_cornerScore_histogram(h_derivative_x,h_derivative_y,h_cornerScore);
+  
+  std::vector<recob::Corner> corner_vector;
+  TH2D *h_maxSuppress;
+  perform_maximum_suppression(h_cornerScore,corner_vector,view,h_maxSuppress);
+
+  //std::vector<recob::Corner> corner_pathIntegralScore_vector;
+  //TH2F *h_pathIntegralScore;
+  //calculate_path_integral_score(h_wire_data,corner_vector,corner_pathIntegralScore_vector,h_pathIntegralScore)
+    
+}
+
---
+++ ClusterFinder/CornerFinderAlg.h
@@ -0,0 +1,94 @@
+////////////////////////////////////////////////////////////////////////
+/// \file  CornerFinderAlgAlg.h
+/// \brief algorithm to find feature 2D points
+///
+/// \author  
+////////////////////////////////////////////////////////////////////////
+
+#ifndef CORNERFINDERALG_H
+#define CORNERFINDERALG_H
+
+#include "fhiclcpp/ParameterSet.h" 
+#include "art/Persistency/Common/Ptr.h" 
+#include "art/Persistency/Common/PtrVector.h" 
+#include "TMath.h"
+#include "TH2.h"
+#include <vector>
+#include <string>
+#include "RecoBase/Corner.h"
+
+
+
+namespace cluster { //<---Not sure if this is the right namespace
+
+   class CornerFinderAlg {
+   
+    public:
+    
+    explicit CornerFinderAlg(fhicl::ParameterSet const& pset); 
+    virtual ~CornerFinderAlg();        
+
+    void   reconfigure(fhicl::ParameterSet const& pset);
+    
+    /// \ put the output of the algorithm here
+    /// \ for now I am putting the example of EndPoint
+    /// \ but this is wrong and should be changed
+    
+//    size_t EndPoint(art::PtrVector<recob::Cluster>                 & clusIn, 
+//		    std::vector<recob::EndPoint2D>                 & vtxcol,
+//		    std::vector< art::PtrVector<recob::Hit> >      & vtxHitsOut,
+//		    art::Event                                const& evt,
+//		    std::string                               const& label);
+
+
+
+      TH2F* TakeInRaw(art::PtrVector<raw::RawDigit>	        & rawhits,
+		       art::Event				const&evt);    
+    
+    private:
+    
+    
+    // Need to list the things we will take in from the .fcl file
+    
+    // Taking in RawData from the event
+    
+    std::string  fRawDataLabel;
+    
+    
+    float          fConversion_threshold;
+    int            fConversion_bins_per_input_x;
+    int            fConversion_bins_per_input_y;
+    std::string    fDerivative_method;
+    int            fDerivative_neighborhood;
+    int            fCornerScore_neighborhood;
+    std::string    fCornerScore_algorithm;
+    int            fMaxSuppress_neighborhood;
+    int            fMaxSuppress_threshold;
+    
+     // Making a vector of histograms
+     TH2F* RawData_histos[3];
+     
+     
+    void create_image_histo(TH2F *h_wire_data, TH2F *h_conversion);
+    void create_derivative_histograms(TH2F *h_conversion, TH2F *h_derivative_x, TH2F *h_derivative_y);
+    void create_cornerScore_histogram(TH2F *h_derivative_x, TH2F *h_derivative_y, TH2D *h_cornerScore);
+    size_t perform_maximum_suppression(TH2D *h_cornerScore, 
+				   std::vector<recob::Corner> & corner_vector,
+				   geo::View_t view, 
+				   TH2D *h_maxSuppress);
+				   
+    float path_integral(TH2F *hist, int x1, float y1, int x2, float y2, float threshold);				   
+    
+    size_t calculate_path_integral_score(TH2F* h_wire_data, 
+		      std::vector<recob::Corner> const & corner_vector, 
+		      std::vector<recob::Corner> & corner_pathIntegralScore_vector,
+		      TH2F *h_pathIntegralScore=NULL);
+    void run(TH2F *h_wire_data, geo::View_t view);
+
+
+     };//<---End of class CornerFinderAlg
+
+
+}
+
+#endif //CORNERFINDERALG_H
