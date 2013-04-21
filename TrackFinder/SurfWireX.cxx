246db66db8574893bd075e0f84b472773148af51
Brian Rebel <brebel@fnal.gov>
2013-04-21 14:09:51 +0000

    change call to Geometry::WireIDToWireGeo to accept a const reference to the geo::WireGeo like the method returns rather than instantiating a new copy of a geo::WireGeo.  This prevents some strange memory stomping bug that was showing up
    Former-commit-id: 05b4e464a35ba3fd43df015720cb79348660b650

--- TrackFinder/SurfWireX.cxx
+++ TrackFinder/SurfWireX.cxx
@@ -33,24 +33,20 @@ namespace trkf {
 	
 	std::vector<geo::WireID> vecChannelWireID = geom->ChannelToWire(channel);
 	  
-	//for (auto i=vecChannelWireID.begin(), e=vecChannelWireID.end(); i!=e; ++i ) {
-	//	geo::WireGeo wgeom = geom->WireIDToWireGeo(&i);
-	//}
-    
 	if (vecChannelWireID.size() != 0) {
-		
-		geo::WireGeo wgeom = geom->WireIDToWireGeo(vecChannelWireID[0]);
-
-		// Get wire center and angle from the wire geometry.
-		// Put local origin at center of wire.
-		
-		double xyz[3];
-		wgeom.GetCenter(xyz);
-		double phi = TMath::PiOver2() - wgeom.ThetaZ();
-
-		// Update base class.
-
-		*static_cast<SurfYZPlane*>(this) = SurfYZPlane(xyz[1], xyz[2], phi);
+	  
+	  geo::WireGeo const& wgeom = geom->WireIDToWireGeo(vecChannelWireID[0]);
+	  
+	  // Get wire center and angle from the wire geometry.
+	  // Put local origin at center of wire.
+	  
+	  double xyz[3] = {0.};
+	  wgeom.GetCenter(xyz);
+	  double phi = TMath::PiOver2() - wgeom.ThetaZ();
+	  
+	  // Update base class.
+	  
+	  *static_cast<SurfYZPlane*>(this) = SurfYZPlane(xyz[1], xyz[2], phi);
 	} else {
 		throw cet::exception("Can't Find Wire for that channel") << channel; 
 
