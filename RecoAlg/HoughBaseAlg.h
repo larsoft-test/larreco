2b0a6123561dc3cbac69b88f58559e6177b02a9a
Brian Rebel <brebel@fnal.gov>
2013-10-01 00:58:33 +0000

    a better way to initialize a vector to an empty vector by default in the argument list of a method.  also use the vector::swap method to quickly copy the contents of the input vector into the owned vector
    Former-commit-id: 2079a376f30e5b90b4c6060780c41383d906aa5f

--- RecoAlg/HoughBaseAlg.h
+++ RecoAlg/HoughBaseAlg.h
@@ -29,8 +29,8 @@ namespace recob {
       double p0=0;
       double p1=0;
       houghCorner(double strengthTemp=0,
-          double p0Temp=0,
-          double p1Temp=0)
+		  double p0Temp=0,
+		  double p1Temp=0)
       {
         strength=strengthTemp;
         p0=p0Temp;
@@ -55,12 +55,12 @@ namespace recob {
       int clusterNumber=-999999;
       double showerLikeness=0;
       mergedLines (double totalQTemp=0,
-          double pMin0Temp=0,
-          double pMin1Temp=0,
-          double pMax0Temp=0,
-          double pMax1Temp=0,
-          double clusterNumberTemp=-999999,
-          double showerLikenessTemp=0)
+		   double pMin0Temp=0,
+		   double pMin1Temp=0,
+		   double pMax0Temp=0,
+		   double pMax1Temp=0,
+		   double clusterNumberTemp=-999999,
+		   double showerLikenessTemp=0)
       {
         totalQ=totalQTemp;
         pMin0=pMin0Temp;
@@ -85,12 +85,12 @@ namespace recob {
       int iMaxWire=0;
       bool showerMerged=false;
       showerLine (double slopeTemp=0,
-          double interceptTemp=0,
-          int clusterNumberTemp=-999999,
-          int oldClusterNumberTemp=-999999,
-          int lineSizeTemp=0,
-          int iMinWireTemp=0,
-          int iMaxWireTemp=0)
+		  double interceptTemp=0,
+		  int clusterNumberTemp=-999999,
+		  int oldClusterNumberTemp=-999999,
+		  int lineSizeTemp=0,
+		  int iMinWireTemp=0,
+		  int iMaxWireTemp=0)
       {
         slope=slopeTemp;
         intercept=interceptTemp;
@@ -131,18 +131,18 @@ namespace recob {
       std::vector<std::pair<double,double> > pHitChargeSigma;
       std::vector<art::Ptr<recob::Hit>> hits;
       lineSlope(unsigned int num=999999, 
-          double slope=999999, 
-          double intercept=999999,
-          double totalQTemp=-999999,
-          double Min0=999999, 
-          double Min1=999999, 
-          double Max0=-999999, 
-          double Max1=-999999,
-          int    iMinWireTemp=999999,
-          int    iMaxWireTemp=-999999,
-          int    minWireTemp=999999,
-          int    maxWireTemp=-999999,
-          std::vector<art::Ptr<recob::Hit>> hitsTemp=NULL)
+		double slope=999999, 
+		double intercept=999999,
+		double totalQTemp=-999999,
+		double Min0=999999, 
+		double Min1=999999, 
+		double Max0=-999999, 
+		double Max1=-999999,
+		int    iMinWireTemp=999999,
+		int    iMaxWireTemp=-999999,
+		int    minWireTemp=999999,
+		int    maxWireTemp=-999999,
+		std::vector<art::Ptr<recob::Hit>> const& hitsTemp=std::vector<art::Ptr<recob::Hit>>())
       {
         clusterNumber = num;
         oldClusterNumber = num;
@@ -160,7 +160,7 @@ namespace recob {
         merged = false;
         showerMerged = false;
         showerLikeness = 0;
-        hits = hitsTemp;
+        hits.swap(hitsTemp);
       }
     };
 
@@ -200,7 +200,10 @@ namespace cluster {
          
     int m_dx;
     int m_dy;
-    // Note, m_accum is a vector of associative containers, the vector elements are called by rho, theta is the container key, the number of hits is the value corresponding to the key
+    // Note, m_accum is a vector of associative containers, 
+    // the vector elements are called by rho, 
+    // theta is the container key, 
+    // the number of hits is the value corresponding to the key
     std::vector<std::map<int,int> > m_accum;  // column=rho, row=theta
     int m_rowLength;
     int m_numAccumulated;
@@ -232,22 +235,22 @@ namespace cluster {
 			 art::Event                                const& evt,
 			 std::string                               const& label);
 
+    // clusterId is the id of the cluster we are examining
     size_t Transform(std::vector<art::Ptr<recob::Hit> > const& hits,
-                     std::vector<unsigned int>     *fpointId_to_clusterId,
-                     unsigned int clusterId, // The id of the cluster we are examining
-                     int *nClusters,
-                     std::vector<unsigned int> corners);
+                     std::vector<unsigned int>                *fpointId_to_clusterId,
+                     unsigned int                              clusterId, 
+                     int                                      *nClusters,
+                     std::vector<unsigned int>                 corners);
     
     // interface to look for lines only on a set of hits,without slope and totalQ arrays
     size_t FastTransform(std::vector < art::Ptr < recob::Hit > >                 & clusIn,
-     	             std::vector< art::PtrVector<recob::Hit> >      & clusHitsOut
-		     );
+     	             std::vector< art::PtrVector<recob::Hit> >                   & clusHitsOut);
     
     // interface to look for lines only on a set of hits
     size_t FastTransform(std::vector < art::Ptr < recob::Hit > >                 & clusIn,
-     	             std::vector< art::PtrVector<recob::Hit> >      & clusHitsOut,
-		     std::vector<double> &slope, std::vector<double> &totalQ
-		     );
+			 std::vector< art::PtrVector<recob::Hit> >               & clusHitsOut,
+			 std::vector<double>                                     & slope, 
+			 std::vector<double>                                     & totalQ);
     
 
     size_t Transform(std::vector<art::Ptr<recob::Hit> > const& hits);
@@ -271,78 +274,89 @@ namespace cluster {
     int    fNumAngleCells;                 ///< Number of angle cells in the accumulator 
                                            ///< (a measure of the angular resolution of the line finder). 
                                            ///< If this number is too large than the number of votes 
-                                           ///< that fall into the "correct" bin will be small and consistent with noise.
-    double fMaxDistance;                   ///< Max distance that a hit can be from a line to be considered part of that line
+                                           ///< that fall into the "correct" bin will be 
+                                           ///< small and consistent with noise.
+    double fMaxDistance;                   ///< Max distance that a hit can be from a line to be 
+                                           ///< considered part of that line
     double fMaxSlope;                      ///< Max slope a line can have
-    int    fRhoZeroOutRange;               ///< Range in rho over which to zero out area around previously found lines in the accumulator
-    int    fThetaZeroOutRange;             ///< Range in theta over which to zero out area around previously found lines in the accumulator
+    int    fRhoZeroOutRange;               ///< Range in rho over which to zero out area around 
+                                           ///< previously found lines in the accumulator
+    int    fThetaZeroOutRange;             ///< Range in theta over which to zero out area around 
+                                           ///< previously found lines in the accumulator
     int    fRhoResolutionFactor;           ///< Factor determining the resolution in rho
-    int    fPerCluster;                    ///< Tells the original Hough algorithm to look at clusters individually, or all hits
-                                           ///< at once
-    int    fMissedHits;                    ///< Number of wires that are allowed to be missed before a line is broken up into
-                                           ///< segments
-    double fMissedHitsDistance;            ///< Distance between hits in a hough line before a hit is considered missed
-    double fMissedHitsToLineSize;          ///< Ratio of missed hits to line size for a line to be considered a fake
-    int    fDoFuzzyRemnantMerge;           ///< Tell the algorithm to merge fuzzy cluster remnants into showers or tracks (0-off, 1-on)
+    int    fPerCluster;                    ///< Tells the original Hough algorithm to look at 
+                                           ///< clusters individually, or all hits at once
+    int    fMissedHits;                    ///< Number of wires that are allowed to be missed before 
+                                           ///< a line is broken up into segments
+    double fMissedHitsDistance;            ///< Distance between hits in a hough line before a 
+                                           ///< hit is considered missed
+    double fMissedHitsToLineSize;          ///< Ratio of missed hits to line size for a line to 
+                                           ///< be considered a fake
+    int    fDoFuzzyRemnantMerge;           ///< Tell the algorithm to merge fuzzy cluster remnants
+                                           ///< into showers or tracks (0-off, 1-on)
     int    fDoHoughLineMerge;              ///< Turns on Hough line merging (0-off, 1-on)
-    double fHoughLineMergeAngle;           ///< Max angle between slopes before two lines are merged (muon tracks), only for fuzzy clustering
-    double fShowerHoughLineMergeAngle;     ///< Max angle between slopes before two lines are merged, for lines in shower line regions
+    double fHoughLineMergeAngle;           ///< Max angle between slopes before two lines are merged
+                                           ///<  (muon tracks), only for fuzzy clustering
+    double fShowerHoughLineMergeAngle;     ///< Max angle between slopes before two lines are merged,
+                                           ///<  for lines in shower line regions
     int    fDoShowerHoughLineMerge;        ///< Turns on shower Hough line merging (0-off, 1-on)
                                            ///< for (electron showers), only for fuzzy clustering
-    int    fDoChargeAsymAngleMerge;        ///< Turn on cut on product of charge asymmetry and sin of angle between slopes of lines
-    double fChargeAsymAngleCut;            ///< Cut on product of charge asymmetry and sin of angle between slopes of lines
-    double fSigmaChargeAsymAngleCut;       ///< Cut on product of charge asymmetry and sin of angle between slopes of lines
-    double fChargeAsymAngleCutoff;         ///< Distance between lines before cut on product of charge asymmetry and sin of angle between slopes of lines
+    int    fDoChargeAsymAngleMerge;        ///< Turn on cut on product of charge asymmetry and sin of
+                                           ///<  angle between slopes of lines
+    double fChargeAsymAngleCut;            ///< Cut on product of charge asymmetry and sin of angle
+                                           ///<  between slopes of lines
+    double fSigmaChargeAsymAngleCut;       ///< Cut on product of charge asymmetry and sin of angle
+                                           ///<  between slopes of lines
+    double fChargeAsymAngleCutoff;         ///< Distance between lines before cut on product of 
+                                           ///< charge asymmetry and sin of angle between slopes of lines
                                            ///< is applied
     double fShowerWidthAngle;              ///< Half of the angle defining how wide a shower is 
-    double fHoughLineMergeCutoff;          ///< Max distance between Hough lines before two lines are merged (muon tracks), 
-                                           ///< only for fuzzy clustering
-    double fShowerHoughLineMergeCutoff;    ///< Max distance between Hough lines before two lines are merged (electron showers),
-    int    fDoShowerHoughLineInterceptMerge;///< Turns on Hough line merging for shower like lines, merging if they intercept (0-off, 1-on)
+    double fHoughLineMergeCutoff;          ///< Max distance between Hough lines before two lines 
+                                           ///< are merged (muon tracks), only for fuzzy clustering
+    double fShowerHoughLineMergeCutoff;    ///< Max distance between Hough lines before two lines 
+                                           ///< are merged (electron showers),
+    int    fDoShowerHoughLineInterceptMerge;///< Turns on Hough line merging for shower like lines, 
+                                           ///< merging if they intercept (0-off, 1-on) 
                                            ///< line closest to each other
-    double fShowerLikenessCut;             ///< Cut on shower likeness (larger the more shower like, smaller the less shower like)
+    double fShowerLikenessCut;             ///< Cut on shower likeness (larger the more shower like, 
+                                           ///< smaller the less shower like)
 
 
     void mergeHoughLinesBySegment(unsigned int k,
-        std::vector<lineSlope> *linesFound, 
-        double xyScale,
-        int mergeStyle,
-        double wire_dist,
-        double tickToDist);
-
+				  std::vector<lineSlope> *linesFound, 
+				  double xyScale,
+				  int mergeStyle,
+				  double wire_dist,
+				  double tickToDist);
+    
     //std::vector<lineSlope> linesFound;
     double HoughLineDistance(double p0MinLine1, 
-        double p1MinLine1, 
-        double p0MaxLine1, 
-        double p1MaxLine1, 
-        double p0MinLine2, 
-        double p1MinLine2, 
-        double p0MaxLine2, 
-        double p1MaxLine2);
+			     double p1MinLine1, 
+			     double p0MaxLine1, 
+			     double p1MaxLine1, 
+			     double p0MinLine2, 
+			     double p1MinLine2, 
+			     double p0MaxLine2, 
+			     double p1MaxLine2);
     bool   HoughLineIntersect(double x11,
-        double  y11,
-        double  x12,
-        double  y12,
-        double  x21,
-        double  y21,
-        double  x22,
-        double  y22);
+			      double  y11,
+			      double  x12,
+			      double  y12,
+			      double  x21,
+			      double  y21,
+			      double  x22,
+			      double  y22);
     double PointSegmentDistance(double px,
-        double  py,
-        double  x1,
-        double  y1,
-        double  x2,
-        double  y2);
-
-    double DistanceBetweenHits(
-        art::Ptr<recob::Hit> hit0,
-        art::Ptr<recob::Hit> hit1,
-        double wire_dist,
-        double tickToDist);
-
-
+				double  py,
+				double  x1,
+				double  y1,
+				double  x2,
+				double  y2);
     
-
+    double DistanceBetweenHits(art::Ptr<recob::Hit> hit0,
+			       art::Ptr<recob::Hit> hit1,
+			       double wire_dist,
+			       double tickToDist);
 
   protected:
 
